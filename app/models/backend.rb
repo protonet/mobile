22d0b7930366a82e177d2113a72d2454df843a24
ali <Ali@alibook-5.local>
2008-09-25 21:35:51 +0200

    first version of the backend, not much in here but you get the idea, also the functions will be delegated

--- app/models/backend.rb
+++ app/models/backend.rb
@@ -1,4 +1,17 @@
 class Backend
+  
+  class << self
+    def get_ips_of_currently_connected_clients
+    
+    end
+  
+    def give_internet_rights_to_client(ip)
+    
+    end
+  
+    def rewoke_internet_rights_from_client(ip)
 
-
+    end
+  end
+  
 end
