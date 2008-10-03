6cc174ac3ac2f21d47b778efda29e07e51ee2342
ali <Ali@alibook-5.local>
2008-10-03 16:22:25 +0200

    get mac addresses connected to wifi interface

--- lib/backend_adapters/ubuntu.rb
+++ lib/backend_adapters/ubuntu.rb
@@ -33,7 +33,7 @@ module BackendAdapters
 #    private
       def get_connected_macs_to_wlan(iface)
         raise ArgumentError, iface unless DEFAULT_WLAN_INTERFACES.include?(iface)
-        return `wlanconfig #{@config[iface]} list sta`
+        `wlanconfig #{@config[iface]} list sta`.scan(/..:..:..:..:..:../)
       end  
 
   end
