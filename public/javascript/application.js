7eba6e3ae1dfd2426fc8ecb1908dbef44d14cccf
ali.local <Ali@alibook-5.local>
2008-10-23 18:23:54 +0200

    room viewer works now, user list viewer in the works

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -19,6 +19,11 @@ function ChatWidget(args) {
   this.current_user  = new ChatUser(args.user_id);
   this.user_config   = args.user_config;
   
+  // active informations
+  this.active_room_id = null;
+  this.rooms = {};
+  this.user_lists = {};
+  
   // keep it simple, no init for now
   this.openLobby();
   
@@ -34,7 +39,12 @@ ChatWidget.prototype =
   },   
   "openRoom": function(room_id) {
     this.refreshUserListForRoom(room_id);
-    this.room_viewer.renderFor(room_id);
+    this.active_room_id = room_id;
+    this.room_viewer.setActive(room_id);
+    this.user_list.setActive(room_id);
+    // not yet implemented:
+    // this.room_selector.setActive(room_id);
+    // this.chat_input.setActive(room_id);
     // this.listenToRoom(foobar?);
   },
   "refreshUserListForRoom": function(room_id) {
@@ -44,32 +54,13 @@ ChatWidget.prototype =
   },
   "receiveEventFromDispatcher": function() {
     // this.room_viewer.add_message(new ChatMessage());
-  }
-}
-
-function ChatInput(args) {
-  var self = this;
-  this.parent_widget = args.parent_widget;
-  if(!this.parent_widget)
-    throw 'parent widget not given';
-  this.input_element = $(document.createElement('input'));
-  
-  this._addOwnElementToParent();
-  
-  this.input_element.attr('size', 60) ; 
-}
-
-ChatInput.prototype = {
-  "_addOwnElementToParent": function() {
-    this.parent_widget.div_container.append(this.input_element);
   },
-  "renderFor": function(room_id) {
-    this.active_room_id = room_id;
+  "activeRoom": function() {
+    return this.rooms['room_' + this.active_room_id];
   }
 }
 
-
-function ChatUserList(args) {
+function ChatUserList(room_id, parent_widget) {
   var self = this;
   this.parent_widget = args.parent_widget;
   if(!this.parent_widget)
@@ -86,24 +77,19 @@ ChatUserList.prototype = {
   },
   "renderFor": function(room_id) {
     this.active_room_id = room_id;
-  }
-}
-
-
-function ChatRoomSelector(args) {
-  var self = this;
-  this.parent_widget = args.parent_widget;
-  if(!this.parent_widget)
-    throw 'parent widget not given';  
-  this.selector_element = $(document.createElement('div'));
-  this.selector_element.addClass('chat-room-selector')
-  
-  this._addOwnElementToParent();
-} 
-
-ChatRoomSelector.prototype = {
-  "_addOwnElementToParent": function() {
-    this.parent_widget.div_container.append(this.selector_element);
+  },
+  "createOrReturnUserList": function(room_id) {
+    var room_key = 'room_' + room_id;
+    if(!this.parent_widget.user_lists[room_key]) {
+      this.parent_widget.user_lists[room_key] = new ChatUserList(room_id, this);
+    }
+    return this.parent_widget.user_lists[room_key];
+  },
+  "setActive": function(room_id) {
+    room = this.createOrReturnRoom(room_id);
+    room.getLastMessages(true);
+    this.view_element.append(this.activeRoom().room_element);
+    this.scrollToLast();
   }
 }
 
@@ -116,34 +102,24 @@ function ChatRoomViewer(args) {
   this.view_element.addClass('chat-room-viewer')
   
   this._addOwnElementToParent();
-  
-  this.active_room_id = null;
-  this.rooms = {};
 };
 
 ChatRoomViewer.prototype = {
   "_addOwnElementToParent": function() {
     this.parent_widget.div_container.append(this.view_element);
-  },    
-  "renderFor": function(room_id) {
-    room = this.createOrReturnRoom(room_id);
-    user_list = this;
-    room.getLastMessages(true);
-  },
+  }
   "createOrReturnRoom": function(room_id) {
     var room_key = 'room_' + room_id;
-    if(!this.rooms[room_key]) {
-      this.rooms[room_key] = new ChatRoom(room_id, this);
+    if(!this.parent_widget.rooms[room_key]) {
+      this.parent_widget.rooms[room_key] = new ChatRoom(room_id, this);
     }
-    return this.rooms[room_key];
+    return this.parent_widget.rooms[room_key];
   },
   "setActive": function(room_id) {
-    this.active_room_id = room_id;
+    room = this.createOrReturnRoom(room_id);
+    room.getLastMessages(true);
     this.view_element.append(this.activeRoom().room_element);
     this.scrollToLast();
-  },
-  "activeRoom": function() {
-    return this.rooms['room_' + this.active_room_id];
   },
   "scrollToLast": function() {
     this.view_element.scrollTo(this.activeRoom().room_element.children('.message:last'));
@@ -164,7 +140,7 @@ function ChatRoom(room_id, parent_widget) {
 }
 
 ChatRoom.prototype = {
-  "getLastMessages": function(active) {
+  "getLastMessages": function(set_active) {
     var self = this;
     if(!this.block_get) {
       $.get("/chat_messages/index", {"room_id": this.room_id, "received_message_ids[]": this.receivedMessageIds()}, function(messages){
@@ -172,7 +148,7 @@ ChatRoom.prototype = {
         for(var i in messages) {
           self.appendMessage(new ChatMessage(messages[i], self));
         }
-        if(active) {
+        if(set_active) {
           self.parent_widget.setActive(self.room_id);
         }
       });
@@ -214,4 +190,42 @@ function ChatUser(args) {
 
 ChatUser.prototype = {
   
+}
+
+function ChatInput(args) {
+  var self = this;
+  this.parent_widget = args.parent_widget;
+  if(!this.parent_widget)
+    throw 'parent widget not given';
+  this.input_element = $(document.createElement('input'));
+  
+  this._addOwnElementToParent();
+  
+  this.input_element.attr('size', 60) ; 
+}
+
+ChatInput.prototype = {
+  "_addOwnElementToParent": function() {
+    this.parent_widget.div_container.append(this.input_element);
+  },
+  "renderFor": function(room_id) {
+    this.active_room_id = room_id;
+  }
+}
+
+function ChatRoomSelector(args) {
+  var self = this;
+  this.parent_widget = args.parent_widget;
+  if(!this.parent_widget)
+    throw 'parent widget not given';  
+  this.selector_element = $(document.createElement('div'));
+  this.selector_element.addClass('chat-room-selector')
+  
+  this._addOwnElementToParent();
+} 
+
+ChatRoomSelector.prototype = {
+  "_addOwnElementToParent": function() {
+    this.parent_widget.div_container.append(this.selector_element);
+  }
 }
