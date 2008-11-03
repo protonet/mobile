c7216882d3debcdea1e7d330c62d7e0a29384856
ali.local <Ali@alibook-8.local>
2008-11-03 23:09:28 +0100

    reworking the chat models, specializing stuff

--- app/models/member.rb
+++
@@ -1,9 +0,0 @@
-class Member
-  include DataMapper::Resource
-  
-  property :id,                         Integer,  :serial => true
-  property :user_id,                    Integer
-  property :room_id,                    Integer
-
-
-end
