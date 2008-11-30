f2176938f0bdfb275a0ef1dc10deac91fbb9748d
ali.local <Ali@alibook-8.local>
2008-11-30 01:40:29 +0100

    fixing last failing spec

--- spec/controllers/instruments_spec.rb
+++ spec/controllers/instruments_spec.rb
@@ -3,9 +3,12 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 describe Instruments, "methods" do
 
   before(:each) do
+    User.all.destroy!
+    @user = User.new(:id => 1, :login => 'foobar')
+    @user.save
     @controller = dispatch_to(Instruments, :index) do |controller|
       controller.should_receive(:login_required).and_return(true)
-      controller.instance_variable_set(:@current_user, User.new(:id => 1, :login => 'foobar'))
+      controller.instance_variable_set(:@current_user, @user)
     end
   end
   
