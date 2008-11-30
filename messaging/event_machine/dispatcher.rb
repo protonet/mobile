21b81801ef1ad9546cdb55f0742def80f06e64ea
Patryk Peszko <patryk.peszko@xing.com>
2008-11-30 18:30:30 +0100

    with dudemeister: awesome eventmachine module hacking!

--- messaging/event_machine/dispatcher.rb
+++ messaging/event_machine/dispatcher.rb
@@ -1,32 +1,45 @@
 #!/usr/bin/env ruby
-
 require 'rubygems'
 require 'eventmachine'
+require 'ruby-debug'
+Debugger.start
 
 # should be requireable to include basic flash interaction functionality
 # policy stuff for example ...
 # aj: maybe some monkeypatching of the the receive data sutff
 # also can include / require the modules class variables?
 module FlashServer
+  
   @policy_sent = false
   
-  def receive_data(data)
-    return send_swf_policy unless @policy_sent
+  def self.included(base)
+    base.class_eval do
+      include InstanceMethods
+      alias :receive_data_without_policy_handler :receive_data
+      alias :receive_data :receive_data_with_policy_handler
+    end
   end
   
-  # this is a flash security policy thing that needs to be sent on the first request to
-  # this server
-  def send_swf_policy
-    policy = <<-EOS
-    <?xml version="1.0" encoding="UTF-8"?> 
-    <cross-domain-policy xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.adobe.com/xml/schemas/PolicyFileSocket.xsd">
-        <allow-access-from domain="*" to-ports="*" secure="false" />
-        <site-control permitted-cross-domain-policies="master-only" />
-    </cross-domain-policy>\0
-    EOS
-     @policy_sent && send_data(policy)
+  module InstanceMethods
+
+    def receive_data_with_policy_handler(data)
+      return send_swf_policy unless @policy_sent
+      receive_data_without_policy_handler(data)
+    end
+    
+    # this is a flash security policy thing that needs to be sent on the first request to
+    # this server
+    def send_swf_policy
+      policy = <<-EOS
+      <?xml version="1.0" encoding="UTF-8"?> 
+      <cross-domain-policy xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.adobe.com/xml/schemas/PolicyFileSocket.xsd">
+          <allow-access-from domain="*" to-ports="*" secure="false" />
+          <site-control permitted-cross-domain-policies="master-only" />
+      </cross-domain-policy>\0
+      EOS
+       (@policy_sent = true) && send_data(policy)
+    end    
   end
-  
 end
 
 module EchoServer
@@ -39,13 +52,14 @@ module EchoServer
     send_data(answer)
   end
   
-  
+  include FlashServer # move me up if you know how!
+
 end
 
 EventMachine::run do
   host = '0.0.0.0'
   port = 5000
-  EventMachine.epoll if RUBY_PLATFORM =~ /linux/
+  EventMachine.epoll if RUBY_PLATFORM =~ /linux/ #sky is the limit
   EventMachine::start_server(host, port, EchoServer)
   puts "Started EchoServer on #{host}:#{port}..."
 end
