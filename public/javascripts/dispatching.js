8ff5e43d18bf26cc5dbf56e3072372b38d77d365
ali.local <Ali@alibook-8.local>
2008-12-06 21:16:20 +0100

    fixing messaging js dispatcher

--- public/javascripts/dispatching.js
+++ public/javascripts/dispatching.js
@@ -1,41 +1,46 @@
-function Dispatcher() {
+function DispatchingSystem() {}
+   
+DispatchingSystem.prototype = {
   "initSocket": function(socket_object) {
     this.socket = socket_object;
   },
-  
+
   // destination_id is your key for eventmachine/js communication
   // e.g. eventmachine sends an destination_id with each message
   // and the dispatcher finds the correct destination for this
   "addRecipient": function(destination_id, recipient_object) {
-    
-  },
   
+  },
+
   // maybe not needed will see
   "removeRecipient": function(destination_id) {
-    
-  },
   
+  },
+
   "findDestination": function(destination_id) {
-    
-  },
   
+  },
+
   "receiveMessage": function(data) {
-    console.log(data + ' wurde empfangen.')
+    console.log(data + ' wurde empfangen.');
     // parsed_message = this.parseMessage(data);
     // this.dispatch(this.findDestination(parsed_message[0]), parsed_message[1])
   },
-  
+
   "parseMessage": function(data) {
-    
-  },
   
+  },
+
   "sendMessage": function(data) {
-    console.log('Versuche ' + data + ' zu senden.')
+    console.log('Versuche ' + data + ' zu senden.');
     this.socket.socket_send(data);
   },
-  
+
   "dispatch": function(to, data) {
     // ;)
     to(data);
   }
+
 }
+
+var Dispatcher = new DispatchingSystem();
