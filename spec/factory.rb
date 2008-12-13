10ebf72aeabe9811458be9d0367fa2b2942f0c18
ali.u100 <jelveh@jelveh-u100.(none)>
2008-12-13 22:11:48 +0100

    adding factory configs

---
+++ spec/factory.rb
@@ -0,0 +1,22 @@
+# user fixtures
+User.fixture(:complete) {{
+  :login                => (login = /\w+/.gen),
+  :email                => "#{name}@example.com",
+  :password             => (password = /\w+/.gen),
+  :pasword_confirmation => password
+}}
+  
+User.fixture {{
+  :login                => (login = /\w+/.gen),
+  :password             => "#{login}@example.com",
+}}
+
+# chat room fixtures
+ChatRoom.fixture {{
+  :name                 => (name = /\w+/.gen)
+}}
+
+# chat message fixtures
+ChatMessage.fixture {{
+  :text                 => (text = /\w+/.gen)
+}}
