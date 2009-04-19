36ad5cf7051a290b48a7a4e6c76a45afc5af6a03
ali.local <Ali@alibook-10.local>
2009-04-19 18:33:08 +0200

    the activity of the audience is correclty toggled too now

--- app/javascripts/communication_console.js
+++ app/javascripts/communication_console.js
@@ -36,16 +36,17 @@ function AudienceSelector(args) {
   
   // get container
   this.container = $('#audience');
-  // bind clicks
   
   // get feed-holder
   this.feed_holder =$('#feed-holder');
-  
+    
+  // get all audience links and bind click
   this.container.find('.audience a').click(function(){
     // get the index of the element
     var index = parseInt(this.href.match(/index=([0-9]*)/)[1]);
     self.feed_holder.animate({'left': index * -604}, 'slow');
+    self.container.find('.active').toggleClass('active');
+    $(this).parent('li').toggleClass('active')
     return false;
-    
   });
 }
