dee377a3c38a3a0f1014c476359f93e6bd9e13fe
ali.local <Ali@alibook-10.local>
2009-05-02 22:07:01 +0200

    fixing flash socket creation, needs cleanup at some point

--- app/javascripts/dispatching/dispatching.js
+++ app/javascripts/dispatching/dispatching.js
@@ -1,6 +1,6 @@
 function DispatchingSystem(server, user_auth_token, user_id) {
   // this.socket = socket;
-  this.createSocket;
+  this.createSocket();
   this.server = server;
   this.user_auth_token = user_auth_token;
   this.user_id = user_id;
@@ -8,10 +8,11 @@ function DispatchingSystem(server, user_auth_token, user_id) {
    
 DispatchingSystem.prototype = {
   
+  // todo: create socket doesn't look as good as it should/could ;)
   "createSocket": function() {
-    // var socket = '<!-- MESSAGING SOCKET --><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="1" height="1" align="middle"><param name="allowScriptAccess" value="sameDomain"><param name="movie" value="flash/socket.swf"><param name="quality" value="high"><param name="bgcolor" value="#FFFFFF">        <embed id="flash_socket" src="/flash/socket.swf" quality="high" bgcolor="#FFFFFF" width="1" height="1" name="flash_socket" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">    </object><!-- END OF MESSAGING SOCKET -->';
-    // $('body').append(socket);
-    // this.socket = document.getElementById('flash_socket');
+    var socket = '<!-- MESSAGING SOCKET --><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="1" height="1" align="middle"><param name="allowScriptAccess" value="sameDomain"><param name="movie" value="flash/socket.swf"><param name="quality" value="high"><param name="bgcolor" value="#FFFFFF">        <embed id="flash_socket" src="/flash/socket.swf" quality="high" bgcolor="#FFFFFF" width="1" height="1" name="flash_socket" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">    </object><!-- END OF MESSAGING SOCKET -->';
+    $('body').append(socket);
+    this.socket = document.getElementById('flash_socket');
   },
   
   "socketReadyCallback": function() {
