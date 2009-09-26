e8ee885fcd495d5436249968126053b84862c515
Ali Jelveh <jelveh@web.de>
2009-09-26 15:02:11 +0200

    only iphonify password field when it exists

--- app/javascripts/instruments.js
+++ app/javascripts/instruments.js
@@ -10,17 +10,17 @@ var Dispatcher    = new DispatchingSystem(config.dispatching_server, config.toke
 $('document').ready(function() {
   file_widget = new FileWidget();
   
-
-  var registration_password_confirmation_field = $('#new-user-password-confirmation');
-  var registration_password_field = $('#new-user-password');
-
-  // iphonify password field
-  registration_password_field.dPassword();
+  // iphonify password field 
+  var registration_password_field = $('#new-user-password');  
+  if(registration_password_field) {
+    // user creation copy the password field for the confirmation thing
+    var registration_password_confirmation_field = $('#new-user-password-confirmation');    
+    registration_password_field.dPassword();
+    $('#registration-form').submit(function(){
+      registration_password_confirmation_field.val(registration_password_field.val());
+    });    
+  }
   
-  // user creation copy the password field for the confirmation thing
-  $('#registration-form').submit(function(){
-    registration_password_confirmation_field.val(registration_password_field.val());
-  });
   // 
 });
 
