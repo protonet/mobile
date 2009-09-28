1c9055eefeabeb717071cfbb715af53e67875571
Ali Jelveh <jelveh@web.de>
2009-09-28 18:44:20 +0200

    blubb

--- app/javascripts/instruments.js
+++ app/javascripts/instruments.js
@@ -4,23 +4,24 @@
 //= require "file_widget/file_widget.js"
 //= require "lib/jQuery.dPassword.js"
 //= require "navigation.js"
+//= require "lib/swfupload.js"
 
 var cc = new CommunicationConsole({'config': config});
 var Dispatcher    = new DispatchingSystem(config.dispatching_server, config.token, config.user_id);
 $('document').ready(function() {
+  
   file_widget = new FileWidget();
   
   // iphonify password field 
-  var registration_password_field = $('#new-user-password');  
+  var registration_password_field = $('#new-user-password');
   if(registration_password_field) {
     // user creation copy the password field for the confirmation thing
-    var registration_password_confirmation_field = $('#new-user-password-confirmation');    
+    var registration_password_confirmation_field = $('#new-user-password-confirmation');
     registration_password_field.dPassword();
     $('#registration-form').submit(function(){
       registration_password_confirmation_field.val(registration_password_field.val());
-    });    
+    });
   }
-  
-  // 
+
 });
 
