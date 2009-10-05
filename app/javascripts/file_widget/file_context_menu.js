2d2e5cf79951e6f93dbb8a218f69b9f3541c9cbf
Christopher Blum <christopher.blum@xing.com>
2009-10-05 21:22:48 +0200

    Minor improvements for the file list (unified tabbing, made list elements focusable, etc)

--- app/javascripts/file_widget/file_context_menu.js
+++ app/javascripts/file_widget/file_context_menu.js
@@ -1,27 +1,28 @@
 protonet.controls.FileWidget.prototype.FileContextMenu = function(parent) {
   this.parent = parent;
   this.initialize();
-}
+};
 
 protonet.controls.FileWidget.prototype.FileContextMenu.prototype = {
   "initialize": function() {
     var self = this;
-    $("#file-list li").contextMenu({
+    $("#file-list li.file").contextMenu({
       menu: "file-list-menu"
     },
-      function(action, el, pos) {
-        var current_file_path = self.parent.current_path + '/' + el.html();
-        switch(action)
-        {
-        case 'download':
-          console.log('download: ' + current_file_path);
-          document.location = "system/files/show?file_path=" + encodeURIComponent(current_file_path);
-          break;
-        case 'delete':
-          $.post('system/files/delete', {"file_path": current_file_path});
-          console.log('delete: ' + current_file_path);
-          break;
-        }
+    function(action, el, pos) {
+      var current_file_path = self.parent.current_path + '/' + el.html();
+      switch(action)
+      {
+      case 'download':
+        console.log('download: ' + current_file_path);
+        document.location = "system/files/show?file_path=" + encodeURIComponent(current_file_path);
+        break;
+      case 'delete':
+        $.post('system/files/delete', {"file_path": current_file_path});
+        el.remove();
+        console.log('delete: ' + current_file_path);
+        break;
+      }
     });
   }
-}
+};
