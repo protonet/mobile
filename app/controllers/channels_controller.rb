4965f65f0872b8b457d678e43fc7d8a89f06d799
m0rk <mash@zarch.org>
2009-10-13 20:52:56 +0200

    added the possibility to add channels

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -6,6 +6,17 @@ class ChannelsController < ApplicationController
     @channels = Channel.all
   end
   
+  def create
+    channel = Channel.new(params[:channel])
+    success = channel && channel.save
+    if success && channel.errors.empty?
+      flash[:notice] = "Successfully created channel '#{params[:channel][:name]}'"
+    else
+      flash[:error] = "Could not create channel '#{params[:channel][:name]}'"
+    end
+    redirect_to :action => 'index'
+  end
+  
   def search
     @channels = Channel.all(:conditions => ["description LIKE ?", "%#{params[:description]}%"])
     render :index
