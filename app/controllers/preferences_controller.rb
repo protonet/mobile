7b5bf9cb8ee2db5be0ff8383c1846efea046465b
Ali Jelveh <jelveh@web.de>
2009-11-02 22:23:39 +0100

    preferences are now clickable

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,8 +1,22 @@
 class PreferencesController < ApplicationController
   
   def index
-    @preferences = [{:url => 'foobar', :name => 'your profile'}, {:url => 'networks', :name => 'network settings'}]
+    @preferences = [{:url => 'profile', :name => 'your profile'},
+      {:url => 'network_settings', :name => 'network settings'},
+      {:url => 'wifi_settings', :name => 'wifi settings'}]
+  end
+  
+  def profile
+    render :partial => 'profile'
+  end
+  
+  def network_settings
     @network_interfaces = System::Backend.get_interfaces
+    render :partial => 'network_settings'
+  end
+  
+  def wifi_settings
+    render :partial => 'wifi_settings'
   end
   
 end
