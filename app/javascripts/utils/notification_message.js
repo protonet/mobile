de426249c305ce25c21a32746b5b7c38005e7203
m0rk <mash@zarch.org>
2009-11-04 19:08:36 +0100

    flash message javascript is now more versatile (height now gets calculated for the animation)
    -- click on the flash-message now removes the message immediately

--- app/javascripts/utils/notification_message.js
+++ app/javascripts/utils/notification_message.js
@@ -1,31 +1,31 @@
 protonet.utils.NotificationMessage = function() {
+  this.element = $("div.message");
+  
   this._observe();
 };
 
 protonet.utils.NotificationMessage.prototype = {
-  ELEMENT: $("div.message"),
-  
   _observe: function() {
-    if (this.ELEMENT.size() > 0) {
-      setTimeout(function() {
-        this._show();
-      }.bind(this), 500);
+    if (this.element.size() > 0) {
+      this._height = parseInt(this.element.css("height"), 10) + 5;
       
-      setTimeout(function() {
-        this._hide();
-      }.bind(this), 5500);
+      this.element.css("top", -this._height + "px");
+      
+      this.element.click(this._hide.bind(this));
+      setTimeout(this._show.bind(this), 500);
+      setTimeout(this._hide.bind(this), 5500);
     }
   },
   
   _show: function() {
-    this.ELEMENT.animate({
+    this.element.animate({
       top: "0px",
     }, 150);
   },
   
   _hide: function() {
-    this.ELEMENT.animate({
-      top: "-50px",
+    this.element.animate({
+      top: -this._height + "px",
     }, 150);
   }
 };
