6f95fb55c3c96cb50a99ff7b38035e64a856ae5a
Christopher Blum <christopher.blum@xing.com>
2009-11-09 21:23:20 +0100

    Another small css fix + js structuring

--- app/javascripts/instruments.js
+++ app/javascripts/instruments.js
@@ -7,34 +7,45 @@
 //= require "lib/jQuery.dPassword.js"
 
 
-// Initialize 
+
+//---------------------------- INITIALIZE INSTRUMENTS ----------------------------
+
+// Initialize communication stuff
 $(function() {
   new protonet.controls.CommunicationConsole({"config": protonet.config});
   new DispatchingSystem(protonet.config.dispatching_server, protonet.config.token, protonet.config.user_id);
 });
 
+
+// Initialize file stuff
 $(function() {
-  var file_widget = new protonet.controls.FileWidget();
-  var registration_password_field = $('#new-user-password');
-  if(registration_password_field.length == 1) {
-    registration_password_field.dPassword({
-      "ICON_PATH": "images/lock.png",
-      "ICON_STYLES": {
-        display: "inline",
-        position: "absolute",
-        width: "16px", height: "16px",
-        margin: "3px 0 0 -25px",
-        overflow: "hidden", cursor: "pointer",
-        backgroundRepeat: "no-repeat"
-      }
-    });
-    new protonet.utils.InlineHint(registration_password_field, "password");
-    
-    
-    // user creation copy the password field for the confirmation thing
-    var registration_password_confirmation_field = $('#new-user-password-confirmation');
-    $('#registration-form').submit(function(){
-      registration_password_confirmation_field.val(registration_password_field.val());
-    });
+  new protonet.controls.FileWidget();
+});
+
+
+// Initialize password stuff
+$(function() {
+  var registration_password_field = $("#new-user-password");
+  if (registration_password_field.length < 1) {
+    return;
   }
+  
+  registration_password_field.dPassword({
+    "ICON_PATH": "images/lock.png",
+    "ICON_STYLES": {
+      display: "inline",
+      position: "absolute",
+      width: "16px", height: "16px",
+      margin: "2px 0 0 -25px",
+      overflow: "hidden", cursor: "pointer",
+      backgroundRepeat: "no-repeat"
+    }
+  });
+  new protonet.utils.InlineHint(registration_password_field, "password");
+  
+  // user creation copy the password field for the confirmation thing
+  var registration_password_confirmation_field = $('#new-user-password-confirmation');
+  $('#registration-form').submit(function(){
+    registration_password_confirmation_field.val(registration_password_field.val());
+  });
 });
