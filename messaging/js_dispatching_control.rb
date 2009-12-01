fffd88eaa682b630f65184144645c7f9b3c2f1ae
Ali Jelveh <jelveh@web.de>
2009-12-01 21:25:23 +0100

    fixing path for the js dispatching control

--- messaging/js_dispatching_control.rb
+++ messaging/js_dispatching_control.rb
@@ -1,5 +1,6 @@
 require 'rubygems'        # if you use RubyGems
 require 'daemons'
 
-Daemons.run(File.join(File.dirname(__FILE__), 'event_machine/js_dispatching_server.rb'), {:dir_mode => :normal, :dir => '/home/protonet/dashboard/shared/pids'})
+shared_path = File.exists?("/var/www/protonet-dashboard/shared/user-files") ? "/var/www/protonet-dashboard/shared/user-files" : "/home/protonet/dashboard/shared/pids"
+Daemons.run(File.join(File.dirname(__FILE__), 'event_machine/js_dispatching_server.rb'), {:dir_mode => :normal, :dir => shared_path})
 
