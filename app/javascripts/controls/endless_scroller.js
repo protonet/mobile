c39cbaee7e4ad762c6c1b3f65554eb8e8a91d90c
Ali Jelveh <jelveh@web.de>
2010-01-15 16:07:59 +0100

    don't continue to load if we have reached the end

--- app/javascripts/controls/endless_scroller.js
+++ app/javascripts/controls/endless_scroller.js
@@ -2,12 +2,12 @@ protonet.controls.EndlessScroller = (function() {
 
   function EndlessScroller(args) {
     this.loading = false;
+    this.end_reached = false;
     $(window).scroll(function () { 
       var window_size = $(document).height();
       // where are we?
       var position_from_top = $(window).scrollTop();
-      if(!this.loading && (window_size - position_from_top < 1100)) {
-        console.log("load new stuff");
+      if(!this.end_reached && !this.loading && (window_size - position_from_top < 1100)) {
         this.loading = true;
         this.loadNewTweets();
       }
@@ -16,14 +16,18 @@ protonet.controls.EndlessScroller = (function() {
   
   EndlessScroller.prototype = {
     "loadNewTweets": function() {
-      var elements = $("#messages-for-channel-1 li");
+      var elements = $("#messages-for-channel-1 > li");
       first_tweet = elements[elements.size()-1];
       $.get("/tweets", {channel_id : 1, first_id: first_tweet.id.match(/tweet-(.*)/)[1]}, function(data) {
-        $("#messages-for-channel-1").append(data);
-        this.loading = false;
+        if(data=="\n") {
+          this.end_reached = true;
+        } else {
+          $("#messages-for-channel-1").append(data);
+          protonet.controls.TextExtension.renderQueue();
+          protonet.controls.PrettyDate.update();          
+        }
         
-        protonet.controls.TextExtension.renderQueue();
-        protonet.controls.PrettyDate.update();
+        this.loading = false;
         
       }.bind(this));
     }
