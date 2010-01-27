c5400539dcebc117613112c6e7a6ddd4c4808dd2
Ali Jelveh <jelveh@web.de>
2010-01-27 20:08:25 +0100

    * gist textextension draft version

---
+++ app/javascripts/controls/text_extension/providers/gist.js
@@ -0,0 +1,63 @@
+//= require "../../../data/yql.js"
+
+/**
+ * XING Profile Provider
+ */
+protonet.controls.TextExtension.providers.GIST = function(url) {
+  this.url = url;
+  // http://gist.github.com/286785
+  this._regExp = /gist\.github\.com\/([0-9]*)/i;
+};
+
+protonet.controls.TextExtension.providers.GIST.prototype = {
+  match: function() {
+    return this._regExp.test(this.url);
+  },
+  
+  _extractId: function() {
+    return this.url.match(this._regExp)[1];
+  },
+  
+  loadData: function(onSuccessCallback, onEmptyResultCallback, onErrorCallback) {
+    // debugger;
+    this.data = {
+      description:  "",
+      title:        "",
+      type:         "GIST",
+      url:          this.url
+    };
+    onSuccessCallback(this.data);
+  },
+  
+  setData: function(data) {
+    // debugger;
+    this.data = data;
+  },
+    
+  getDescription: function() {
+    // var description = this.data.description;
+    // description = description || this.url;
+    // return String('description').truncate(200);
+    return "";
+  },
+  
+  getTitle: function() {
+    // var title = this.data.title;
+    // return String('title').truncate(75);
+    return "";
+  },
+  
+  getMedia: function() {
+    // 286785
+    // http://gist.github.com/286785.js
+    // return $("<script src=\"http://gist.github.com/" + this._extractId() + ".js\"></script>");
+    // jQuery.getScript("http://gist.github.com/" + this._extractId() + ".js");
+    return $("<iframe />", {
+      src: "javascript:'document.write(\"<html>" + "<script src=\"http://gist.github.com/" + this._extractId() + ".js\"></script>" + "</html>'\");"
+    });
+  },
+  
+  cancel: function() {
+    this._canceled = true;
+  }
+};
