4e7224da904dd7cc90935d6fa1b805f75344324b
arnski <arnski@arnski.de>
2010-02-01 20:56:39 +0100

    Prefs: Statechange on click, Mousepointer for Buttons

--- app/javascripts/preferences.js
+++ app/javascripts/preferences.js
@@ -1,6 +1,8 @@
 $(function() {
   $("#preferences ul li").click(function(event){
     $("#preferences-details").load("/preferences/" + event.currentTarget.id)
+    $("#preferences ul li.clicked").toggleClass("clicked");
+    $(this).toggleClass("clicked");
   });
   $("#profile").click();
 });
