3ce8b055b0f2df79581a24292258b72e60a9cdf3
Ali Jelveh <jelveh@web.de>
2010-02-06 16:38:21 +0100

    * js dispatching control work

--- messaging/js_dispatching_control.rb
+++ messaging/js_dispatching_control.rb
@@ -1,6 +1,13 @@
 require 'rubygems'        # if you use RubyGems
 require 'daemons'
 
-shared_path = File.exists?("/var/www/protonet-dashboard/shared/user-files") ? "/var/www/protonet-dashboard/shared/user-files" : "/home/protonet/dashboard/shared/pids"
-Daemons.run(File.join(File.dirname(__FILE__), 'event_machine/js_dispatching_server.rb'), {:dir_mode => :normal, :dir => shared_path})
+shared_path = File.exists?("/var/www/protonet-dashboard/shared") ? "/var/www/protonet-dashboard/shared" : "/home/protonet/dashboard/shared"
+Daemons.run(
+  File.join(File.dirname(__FILE__),'event_machine/js_dispatching_server.rb'),
+  {
+    :dir_mode => :normal,
+    :dir => shared_path + "/pids",
+    :log_output => shared_path + "/log"
+  }
+)
 
