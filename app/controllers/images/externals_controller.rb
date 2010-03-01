b91defe7d0c78d35d19c34ae8fbbaf65df8d81c4
Ali Jelveh <jelveh@web.de>
2010-03-01 21:23:55 +0100

    * now with caching :)

--- app/controllers/images/externals_controller.rb
+++ app/controllers/images/externals_controller.rb
@@ -1,8 +1,8 @@
 class Images::ExternalsController < ApplicationController
-  
+  caches_page :show
   def show
-    @width = params[:width]
-    @height = params[:height]
+    @width = params[:width] unless params[:width] == '0'
+    @height = params[:height] unless params[:height] == '0'
     if params[:image_file_url]
       @external = Images::External.find_or_create_by_image_url(params[:image_file_url])
     elsif params[:id]
