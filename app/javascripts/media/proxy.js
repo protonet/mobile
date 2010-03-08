500f634cac5470d4dd69eea1995308d0d5950625
Ali Jelveh <jelveh@web.de>
2010-03-09 00:12:43 +0100

    * tiff fixed the problem with the apache decoding urls where it shouldn't happen

--- app/javascripts/media/proxy.js
+++ app/javascripts/media/proxy.js
@@ -1,5 +1,7 @@
 protonet.media.Proxy = (function() {
   var imageDefaultSize = { width: 0, height: 0 }, // 0x0 => original size (Logic by Mr. Failveh)
+      // Apache decodes urls before giving them to rails, therefore we need to double encode them
+      DOUBLE_ENCODE_URL = protonet.config.server == "Apache",
       IMAGE_URL = "/images/externals/resize/{width}/{height}/{url}",
       IMAGE_AVAILABLE_URL = "/images/externals/is_available/{url}";
   
@@ -8,7 +10,7 @@ protonet.media.Proxy = (function() {
    * callback is invoked with boolean status parameter
    */
   function isImageAvailable(url, callback) {
-    $.getJSON(IMAGE_AVAILABLE_URL.replace("{url}", encodeURIComponent(url)), function(response) {
+    $.getJSON(IMAGE_AVAILABLE_URL.replace("{url}", _prepareUrl(url)), function(response) {
       callback(response.is_available);
     });
   }
@@ -20,9 +22,17 @@ protonet.media.Proxy = (function() {
   function getImageUrl(url, size) {
     size = $.extend({}, imageDefaultSize, size);
     return IMAGE_URL
-      .replace("{url}", encodeURIComponent(url))
+      .replace("{url}", _prepareUrl(url))
       .replace("{width}", size.width)
       .replace("{height}", size.height);
+  }
+  
+  function _prepareUrl(url) {
+    url = encodeURIComponent(url);
+    if (DOUBLE_ENCODE_URL) {
+      url = encodeURIComponent(url);
+    }
+    return url;
   }
   
   return {
