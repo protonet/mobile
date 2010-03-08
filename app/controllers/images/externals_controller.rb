1a10f9a0756ff3022ec3c1466b9be69de525fd9a
Ali Jelveh <jelveh@web.de>
2010-03-08 22:02:42 +0100

    * pure awesomeness, fixing the image proxy bug with urls that include reserved characters (like with pageglimpse)

--- app/controllers/images/externals_controller.rb
+++ app/controllers/images/externals_controller.rb
@@ -2,6 +2,17 @@ class Images::ExternalsController < ApplicationController
   caches_page :show
   
   def show
+    # handle apache rewrite magic
+    # & ? ; are reserved characters in URIs if you use them in your url (like /images/externals/resize/280/200/http://foo/bar?size=medium&devkey=1)
+    # apache rewrite will split that url up nicely, this is not something we want in this context, this takes the original URI right out of rack
+    # and handles the parameters manually
+    if match = request.env['REQUEST_URI'].match(/^\/images\/externals\/resize\/(\d*)\/(\d*)\/(.*)/)
+      if match[3] && match[3].match(/\?/)
+        params[:width]  = match[1]
+        params[:height] = match[2]
+        params[:image_file_url] = match[3]
+      end
+    end
     @width = params[:width] unless params[:width] == '0'
     @height = params[:height] unless params[:height] == '0'
     if params[:image_file_url]
