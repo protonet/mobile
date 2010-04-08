2a537be4a7035c9bf36bafe72e9b831342234559
ali.jelveh <ali.jelveh@xing.com>
2010-04-08 17:48:47 +0200

    some fixes in our jabber bridge

--- messaging/event_machine/jabber.rb
+++ messaging/event_machine/jabber.rb
@@ -18,26 +18,29 @@ cp.join("cp@conference.im.xing.com/robot")
 
 
 EM.run do
-  
+
   amq = MQ.new
   channel_queue = amq.queue("consumer-jabber-bridge", :auto_delete => true)
+  channel_queue2 = amq.queue("consumer-jabber-bridge2", :auto_delete => true)
+
   channel_queue.bind(amq.topic("channels"), :key => "channels.a#{4}").subscribe do |msg|
     message = JSON(msg)
     askrails.say("#{message["author"]}{p}: #{message["message"]}") unless message["author"].match(/\{x\}/)
   end
-  channel_queue.bind(amq.topic("channels"), :key => "channels.a#{16}").subscribe do |msg|
+
+  channel_queue2.bind(amq.topic("channels"), :key => "channels.a#{16}").subscribe do |msg|
     message = JSON(msg)
     cp.say("#{message["author"]}{p}: #{message["message"]}") unless message["author"].match(/\{x\}/)
   end
   
   EM::PeriodicTimer.new(1) do
-    
-    cp.on_message do |time,user_name,msg|
-      user_name = user_name.downcase.gsub(/ /, '.')
+
+    cp.on_message do |time,nick,msg|
+      user_name = user_from_nickname(nick)
       if(!time && !msg.match(/\{p\}/)) && !msg.match(/\{x\}/)
         begin
           user = User.find_by_login(user_name)
-          tweet = Tweet.new({:author => "#{user_name}{x}", :user => user, :channels => Channel.find([4]), :message => msg.to_s})
+          tweet = Tweet.new({:author => "#{user_name}{x}", :user => user, :channels => Channel.find([16]), :message => msg.to_s})
           tweet.socket_id = '0'
           tweet.save
         rescue Exception => e
@@ -46,9 +49,9 @@ EM.run do
         end
       end
     end
-    
-    askrails.on_message do |time,user_name,msg|
-      user_name = user_name.downcase.gsub(/ /, '.')
+
+    askrails.on_message do |time,nick,msg|
+      user_name = user_from_nickname(nick)
       if(!time && !msg.match(/\{p\}/)) && !msg.match(/\{x\}/)
         begin
           user = User.find_by_login(user_name)
@@ -63,4 +66,19 @@ EM.run do
     end
     
   end
+end
+
+def user_from_nickname(nick)
+  nick.downcase!
+  [{
+    'Wolfram Müller-Grabellus' => 'wolfram.mgrabellus',
+    'Ralph von der Heyden'     => 'ralph.heyden',
+    'ü' => 'ue',
+    'ö' => 'oe',
+    'ä' => 'ae'
+  }].each do |k,v|
+    nick.sub!(k, v)
+  end
+  
+  nick.gsub(/ /, '.')
 end
