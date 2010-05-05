6ca779b18a40ca41ce3d8caba91fec70b3dc4a01
Alex Kulikov <alex.kulikov@gmail.com>
2010-05-05 20:45:40 +0200

    Channellist improvements

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -2,9 +2,9 @@ module ChannelsHelper
 
   def channel_listen_control(channel)
     if current_user.channels.include?(channel)
-      link_to('stop listening', current_user.listens.find_by_channel_id(channel.id), :class => 'listen off', :method => :delete)
+      link_to('<p id="channel-listener"> stop listening </p>', current_user.listens.find_by_channel_id(channel.id), :class => 'listen off', :method => :delete)
     else
-      link_to('start listening', listens_path(:channel_id => channel.id), :class => 'listen on', :method => :post)
+      link_to('<p id="channel-listener"> start listening </p>', listens_path(:channel_id => channel.id), :class => 'listen on', :method => :post)
     end
   end
 
