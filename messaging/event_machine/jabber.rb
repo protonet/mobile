e66fd628e2a7372a3a8b4b3bce0ce0291ff63f9c
ali.jelveh <ali.jelveh@xing.com>
2010-05-10 14:28:16 +0200

    add content discovery room to jabber bridge

--- messaging/event_machine/jabber.rb
+++ messaging/event_machine/jabber.rb
@@ -32,12 +32,16 @@ askrails.join("askrails@conference.im.xing.com/robot")
 cp = Jabber::MUC::SimpleMUCClient.new(jabber.client)
 cp.join("cp@conference.im.xing.com/robot")
 
+content_discovery = Jabber::MUC::SimpleMUCClient.new(jabber.client)
+content_discovery.join("contentdiscovery@conference.im.xing.com/robot")
+
 
 EM.run do
 
   amq = MQ.new
   channel_queue = amq.queue("consumer-jabber-bridge", :auto_delete => true)
   channel_queue2 = amq.queue("consumer-jabber-bridge2", :auto_delete => true)
+  channel_content_discovery = amq.queue("consumer-jabber-bridge3", :auto_delete => true)
 
   channel_queue.bind(amq.topic("channels"), :key => "channels.#{4}").subscribe do |msg|
     message = JSON(msg)
@@ -48,7 +52,12 @@ EM.run do
     message = JSON(msg)
     cp.say("#{message["author"]}{p}: #{message["message"]}") unless message["author"].match(/\{x\}/)
   end
-  
+
+  channel_content_discovery.bind(amq.topic("channels"), :key => "channels.#{21}").subscribe do |msg|
+    message = JSON(msg)
+    content_discovery.say("#{message["author"]}{p}: #{message["message"]}") unless message["author"].match(/\{x\}/)
+  end
+
   EM::PeriodicTimer.new(1) do
 
     cp.on_message do |time,user_name,msg|
@@ -80,7 +89,22 @@ EM.run do
         end
       end
     end
-    
+
+    content_discovery..on_message do |time,user_name,msg|
+      user_name = user_from_nickname(user_name)
+      if(!time && !msg.match(/\{p\}/)) && !msg.match(/\{x\}/)
+        begin
+          user = User.find_by_login(user_name)
+          tweet = Tweet.new({:author => "#{user_name}{x}", :user => user, :channels => Channel.find([21]), :message => msg.to_s})
+          tweet.socket_id = '0'
+          tweet.save
+        rescue Exception => e
+          puts "BAM!"
+          puts e.inspect
+        end
+      end
+    end
+
   end
 end
 
