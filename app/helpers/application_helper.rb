e9c6ce6d1519d18af1ba7ee4c794b1a41c1aa1b3
Ali Jelveh <jelveh@web.de>
2010-05-21 15:01:22 +0200

    * channel linking fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
   
   def highlight_replies(str)
     str.gsub(/(\s|^)@([\w\.\-_@]+)/) {|s|
-      "#{$1}@<span class='reply#{" channel" if Channel.names.include?($2)}'>#{$2}</span>"
+      "#{$1}@<span class='reply#{" channel" if Channel.names.include?($2.downcase)}'>#{$2}</span>"
     }
   end
   
--- node/node.js
+++ node/node.js
@@ -1,6 +1,6 @@
 var sys  = require('sys');
 // var net  = require('net');
-var amqp = require('./../node-amqp/amqp');
+var amqp = require('./../../node-amqp/amqp');
 exec = require('child_process').exec;
 
 var connection = amqp.createConnection({ host: 'localhost' });
@@ -25,6 +25,11 @@ connection.addListener('ready', function () {
             }
           });
         break;
+      // example remove for production
+      case 'eval':
+        var result = eval(message.javascript);
+        user_exchange.publish("users." + message.user_id, {'result': result, 'trigger': 'workdone'});
+        break;
     }
   });
 });
@@ -32,5 +37,8 @@ connection.addListener('ready', function () {
 sys.puts('started');
 
 // how to use node background workers:
+// protonet.globals.notifications.bind('workdone', function(e, msg){ console.log(e, msg) })
 // protonet.globals.dispatcher.sendMessage(JSON.stringify({'operation':'work', 'task':'screenshot'}))
+// or 
+// protonet.globals.dispatcher.sendMessage(JSON.stringify({'operation':'work', 'task':'eval', 'javascript':'2*2'}))
 // and you'll get a message whenever it's ready :)
