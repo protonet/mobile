fdc347be5e58889a9d973e066efcd939111670f5
Ali Jelveh <jelveh@web.de>
2010-05-23 14:52:10 +0200

    * node and webkit2png

--- node/node.js
+++ node/node.js
@@ -15,11 +15,11 @@ connection.addListener('ready', function () {
     sys.p(message)
     switch(message.task) {
       case 'screenshot':
-        exec('ls -al', 
+        exec('./../webkit2png/webkit2png-0.5.sh -d -D node/screenshots ' + message.url, 
           function (error, stdout, stderr) {
             sys.print('stdout: ' + stdout);
-            user_exchange.publish("users." + message.user_id, {'result': stdout});
             sys.print('stderr: ' + stderr);
+            user_exchange.publish("users." + message.user_id, {'result': stdout, 'trigger': 'workdone'});
             if (error !== null) {
               sys.puts('exec error: ' + error);
             }
@@ -38,7 +38,7 @@ sys.puts('started');
 
 // how to use node background workers:
 // protonet.globals.notifications.bind('workdone', function(e, msg){ console.log(e, msg) })
-// protonet.globals.dispatcher.sendMessage(JSON.stringify({'operation':'work', 'task':'screenshot'}))
+// protonet.globals.dispatcher.sendMessage(JSON.stringify({'operation':'work', 'task':'screenshot', 'url':'http://www.google.de'}))
 // or 
 // protonet.globals.dispatcher.sendMessage(JSON.stringify({'operation':'work', 'task':'eval', 'javascript':'2*2'}))
 // and you'll get a message whenever it's ready :)
