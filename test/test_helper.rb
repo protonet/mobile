31d00f7ea0fae6c0544aff1e5930d6f4823f39cf
Ali Jelveh <jelveh@web.de>
2010-06-05 23:57:09 +0200

    blueprints is no more we have factories now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'context'
 require 'mocha'
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 # require 'ruby-debug'
 # Debugger.start
 
