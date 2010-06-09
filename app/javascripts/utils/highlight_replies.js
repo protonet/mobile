557de5a0f9ba9b53f640f27e1c63265dfc6b3078
Burntime <alex.kulikov@gmail.com>
2010-06-09 22:07:17 +0200

    case insensitive @user highlight

--- app/javascripts/utils/highlight_replies.js
+++ app/javascripts/utils/highlight_replies.js
@@ -5,7 +5,7 @@ protonet.utils.highlightReplies = (function() {
     return str.replace(REG_EXP, function(match, $1, $2) {
       if (protonet.globals.channelSelector.channelsDowncaseMapping[$2] || ($.inArray( $2, protonet.globals.channelSelector.channels) != -1)) {
         return $1 + "@" + '<span class="reply channel">' + $2 + '</span>';
-      } else if ($2.match(protonet.config.user_name)) {
+      } else if ($2.toLowerCase().match(protonet.config.user_name.toLowerCase())) {
         return $1 + "@" + '<span class="reply to-me">' + $2 + '</span>';
       } else {
         return $1 + "@" + '<span class="reply ' + $2 + '">' + $2 + '</span>';
