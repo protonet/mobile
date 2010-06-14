90246d1ee05b875ea5c342a19828a26bb04f569c
Tom Kirchner <tokirc@gmx.net>
2010-06-14 21:06:40 +0200

    added flag guest to user db table

---
+++ db/migrate/20100614185346_add_guest_flag_to_user.rb
@@ -0,0 +1,12 @@
+class AddGuestFlagToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :guest, :boolean, :default => true
+    User.find(:all, :conditions => {:admin => true}).each do |user|
+      user.update_attribute(:guest, false)
+    end
+  end
+    
+  def self.down
+    remove_column :users, :guest
+  end
+end
