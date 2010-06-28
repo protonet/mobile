160c9496329683f4903ed47abdb65be8a6f203ea
Christopher Blum <christopher.blum@xing.com>
2010-06-29 00:40:44 +0200

    Minor fix for slideshare text extension

--- app/javascripts/text_extensions/provider/slideshare.js
+++ app/javascripts/text_extensions/provider/slideshare.js
@@ -10,10 +10,11 @@ protonet.text_extensions.provider.Slideshare = {
    * http://www.slideshare.net/nathantwright/fostering-community-with-social-media-midwest-newspaper-summit-2010#test
    * http://www.slideshare.net/nathantwright/fostering-community-with-social-media-midwest-newspaper-summit-2010?a=2
    */
-  REG_EXP: /slideshare\.net\/[\w-]+?\/[\w-]+?($|\?|\#)/i,
+  REG_EXP: /(.+?slideshare\.net\/[\w-]+?\/[\w-]+?)($|\?|\#)/i,
   
   loadData: function(url, onSuccess, onFailure) {
-    protonet.data.SlideShare.getSlideShow(url, function(response) {
+    var match = url.match(this.REG_EXP);
+    protonet.data.SlideShare.getSlideShow(match[1], function(response) {
       onSuccess({
         description:  response.Description,
         image:        response.ThumbnailSmallURL,
