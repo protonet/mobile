b934ca91f783e72e4fce20fce073fb67ad8d62d7
Ali Jelveh <jelveh@web.de>
2010-07-24 22:43:55 +0200

    moving channel selector to a partial and putting one on the files page

--- app/controllers/system/files_controller.rb
+++ app/controllers/system/files_controller.rb
@@ -2,12 +2,15 @@ module System
   class FilesController < ApplicationController
   
     def index
+      @channels = current_user.channels
+      @active_channel = params[:channel_id] ? Channel.find(params[:channel_id]) : @channels.first
+      
       raw_files = FileSystem.all(params['path'])
       @files = raw_files.collect do |k,v|
         {:type => k, :name => v}
       end
       respond_to do |format|
-        format.html # new.html.erb
+        format.html
         format.js  { render :json => @files }
       end
     end
