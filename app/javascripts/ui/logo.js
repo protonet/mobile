ce44d7a8fb625184fd43db2ab07d67554c83764d
Christopher Blum <christopher.blum@xing.com>
2010-08-27 01:58:06 +0200

    Store current monster character in localStorage

--- app/javascripts/ui/logo.js
+++ app/javascripts/ui/logo.js
@@ -3,15 +3,18 @@
  */
 protonet.ui.Logo = {
   monsters:       ["default", "business", "female", "surfer"],
-  monstersIndex:  0,
+  monstersIndex:  (window.localStorage && window.localStorage.monster) || 0,
   
   initialize: function() {
     this.monster = $("header > .monster");
-    this.monster.click(this.changeMonster.bind(this)).queue();
+    this.monster.click(this.nextMonster.bind(this)).queue();
     $(".heart").live("click", this.hearts.bind(this));
+    if (this.monstersIndex != 0) {
+      this.setMonster(this.monsters[this.monstersIndex]);
+    }
   },
   
-  changeMonster: function(event) {
+  nextMonster: function(event) {
     event && event.preventDefault();
     
     if (this.monster.queue().length) {
@@ -23,6 +26,14 @@ protonet.ui.Logo = {
       this.monstersIndex = 0;
     }
     
+    if (window.localStorage) {
+      window.localStorage.monster = this.monstersIndex;
+    }
+    
+    this.setMonster(this.monsters[this.monstersIndex]);
+  },
+  
+  setMonster: function(type) {
     var oldMarginBottom = parseInt(this.monster.css("marginBottom"), 10);
     
     this.monster.animate({
@@ -31,17 +42,17 @@ protonet.ui.Logo = {
       $.each(this.monsters, function(i, className) {
         this.monster.removeClass(className);
       }.bind(this));
-      this.monster.addClass(this.monsters[this.monstersIndex]);
+      this.monster.addClass(type);
     }.bind(this)).animate({
       marginBottom: oldMarginBottom.px()
     }, 250, function() {
-      if (this.monsters[this.monstersIndex] == "female") {
+      if (type == "female") {
         this.hearts();
       }
     }.bind(this));
   },
   
-  hearts: function(event, secondTime) {
+  hearts: function(event) {
     var hearts = $("<span />", {
       className: "hearts",
       html: "&hearts; &hearts; &hearts;<br>&hearts; &hearts;"
@@ -53,11 +64,5 @@ protonet.ui.Logo = {
     }, 2000, function() {
       hearts.remove();
     });
-    
-    if (!secondTime) {
-      setTimeout(function() {
-        this.hearts(null, true);
-      }.bind(this), 500);
-    }
   }
 };
