a2860914bb691e10f15ccbaff2de7961280c80d8
Daniel Danopia <me@danopia.net>
2010-08-27 10:26:22 -0400

    Fix the flash policy sending

--- messaging/event_machine/modules/flash_server.rb
+++ messaging/event_machine/modules/flash_server.rb
@@ -5,17 +5,13 @@ class FlashServer < FlashConnection
   @policy_sent = false
   
   def receive_data data
-    messages = data.split("\0")
-    
     unless @policy_sent
       @policy_sent = true
       
-      if messages[0] == '<policy-file-request/>'
+      if data[0, data.index("\0")] == '<policy-file-request/>'
         send_swf_policy
-        messages.unshift
+        data = data[(data.index("\0")+1)..-1]
       end
-      
-      data = messages.join("\0")
     end
     
     super(data)
@@ -26,12 +22,13 @@ class FlashServer < FlashConnection
   def send_swf_policy
     log "sending policy"
     
-    send_data <<-EOS
+    policy = <<-EOS
     <?xml version="1.0" encoding="UTF-8"?> 
     <cross-domain-policy xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.adobe.com/xml/schemas/PolicyFileSocket.xsd">
         <allow-access-from domain="*" to-ports="*" secure="false" />
         <site-control permitted-cross-domain-policies="master-only" />
     </cross-domain-policy>\0
     EOS
+    send_data policy
   end
 end
