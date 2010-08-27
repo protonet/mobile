c572e10016f8809cd4d0b5f6231240e903ec742b
Daniel Danopia <me@danopia.net>
2010-08-27 09:50:32 -0400

    Reworked a little bit of Flash code

--- messaging/event_machine/modules/flash_server.rb
+++ messaging/event_machine/modules/flash_server.rb
@@ -5,29 +5,33 @@ class FlashServer < FlashConnection
   @policy_sent = false
   
   def receive_data data
-    # log("policy server receiving: #{data}")
     messages = data.split("\0")
+    
     unless @policy_sent
       @policy_sent = true
-      log(messages.inspect)
-      return super(messages[1]) if messages[1]
-      return send_swf_policy if messages[0].match(/policy-file-request/)
+      
+      if messages[0] == '<policy-file-request/>'
+        send_swf_policy
+        messages.unshift
+      end
+      
+      data = messages.join("\0")
     end
-    # log("doing nothing, handing data over to dispatcher")
-    super
+    
+    super(data)
   end
   
   # this is a flash security policy thing that needs to be sent on
   # the first request to the server
   def send_swf_policy
-    log("sending policy")
-    policy = <<-EOS
+    log "sending policy"
+    
+    send_data <<-EOS
     <?xml version="1.0" encoding="UTF-8"?> 
     <cross-domain-policy xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.adobe.com/xml/schemas/PolicyFileSocket.xsd">
         <allow-access-from domain="*" to-ports="*" secure="false" />
         <site-control permitted-cross-domain-policies="master-only" />
     </cross-domain-policy>\0
     EOS
-    send_data(policy)
   end
 end
