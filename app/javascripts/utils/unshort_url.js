4000e7f7c002884ac05c3c12fbe6123d284e8de6
Christopher Blum <christopher.blum@xing.com>
2010-08-29 18:12:35 +0200

    Using non-yql way to get short urls and change from untiny.me to longurl.org which seems to be more performant

--- app/javascripts/utils/unshort_url.js
+++ app/javascripts/utils/unshort_url.js
@@ -1,5 +1,3 @@
-//= require "../data/yql.js"
-
 /**
  * Checks whether the url is of a supported url shortener
  * if not, fire the callback with the original url
@@ -11,41 +9,41 @@
  *    });
  */
 protonet.utils.unshortUrl = (function() {
-  var YQL_QUERY = "SELECT * FROM json WHERE url = 'http://untiny.me/api/1.0/extract/?url={url}&format=json'";
-  
-  var TIMEOUT = 2500; // ms - requests to untiny.me shouldn't take longer than this
-  
-  var SUPPORTED_SERVICES = [
-    "bit.ly",
-    "cli.gs",
-    "fb.me",
-    "ff.im",
-    "flic.kr",
-    "is.gd",
-    "goo.gl",
-    "fav.me",
-    "j.mp",
-    "ow.ly",
-    "snipurl.com",
-    "tcrn.ch",
-    "tinyurl.com",
-    "tr.im"
-  ];
+  var API_URL = "http://api.longurl.org/v2/expand?format=json&callback=?",
+      TIMEOUT = 2500; // ms - requests to longurl.org shouldn't take longer than this
+      SUPPORTED_SERVICES = [
+        "bit.ly",
+        "cli.gs",
+        "fb.me",
+        "ff.im",
+        "flic.kr",
+        "is.gd",
+        "goo.gl",
+        "fav.me",
+        "j.mp",
+        "ow.ly",
+        "snipurl.com",
+        "tcrn.ch",
+        "tinyurl.com",
+        "tr.im"
+      ];
   
   function getLongUrl(url, callback) {
     var failure = function() {
       callback(url);
     };
     
-    var query = YQL_QUERY.replace("{url}", encodeURIComponent(url));
-    
-    new protonet.data.YQL.Query(query).execute(function(response) {
-      if (response.org_url) {
-        callback(response.org_url);
-      } else {
-        failure();
-      }
-    }, failure);
+    $.jsonp({
+      url: API_URL,
+      data: {
+        url: url
+      },
+      success: function(response) {
+        var longUrl = response["long-url"];
+        longUrl ? callback(longUrl) : failure();
+      },
+      error: failure
+    });
   }
   
   return function(url, callback) {
