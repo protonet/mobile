3e01e3a68671b4d70307b9b76d6ca7e838acdab0
Christopher Blum <christopher.blum@xing.com>
2010-09-01 20:57:47 +0200

    Resizing user widget works now

--- app/javascripts/ui/resizer.js
+++ app/javascripts/ui/resizer.js
@@ -1,22 +1,45 @@
-protonet.ui.Resizer = function(element, handle) {
+protonet.ui.Resizer = function(element, handle, options) {
   this.element = element;
   this.handle = handle;
+  this.options = $.extend(options, {
+    callback: $.noop,
+    storage: window.localStorage || {},
+    storageKey: "resize_height"
+  });
+  
+  var storedHeight = this.options.storage[this.options.storageKey];
+  storedHeight && this.element.css("height", storedHeight.px());
+  
   this._observe();
 };
 
 protonet.ui.Resizer.prototype = {
   _observe: function() {
-    this.handle
-      .bind("mousedown", function() {
-        console.log(1);
-      })
-      
-      .bind("mousemove", function() {
-        console.log(2);
-      })
+    var currentPos = null;
+    
+    this.handle.bind("mousedown", function(event) {
+      currentPos = event.pageY;
+    });
+    
+    $("body")
+      .bind("mousemove", function(event) {
+        if (currentPos === null) {
+          return;
+        }
+        
+        var newPos = event.pageY,
+            heightDiff = (newPos - currentPos),
+            newHeight = parseInt(this.element.css("height"), 10) + heightDiff;
+        
+        this.element.css("height", newHeight.px());
+        currentPos = newPos;
+        
+        this.options.storage[this.options.storageKey] = newHeight;
+        this.options.callback(newHeight);
+      }.bind(this))
       
       .bind("mouseup", function() {
-        console.log(3);
+        currentPos = null;
       });
   }
 };
