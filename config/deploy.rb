ac948f4a777fcb62a42de6f279d9ab3ae0b61709
Ali Jelveh <jelveh@web.de>
2010-09-06 19:53:53 +0200

    * make deploy faster, remove schema.rb from gitignore, add readme infos on starting up the app

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,13 +63,13 @@ end
 namespace :bundler do
   task :create_symlink, :roles => :app do
     shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
+    release_dir = File.join(release_path, '.bundle')
     run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
   end
   
   task :bundle_new_release, :roles => :app do
     bundler.create_symlink
-    run "cd #{release_path}; bundle check 2>&1 > /dev/null ; if [ $? -ne 0 ] ; then sh -c 'bundle install --without test --without cucumber' ; fi"
+    run "cd #{release_path}; bundle check 2>&1 > /dev/null ; if [ $? -ne 0 ] ; then sh -c \"bundle install --without test --without cucumber\" ; fi"
   end
   
   task :lock, :roles => :app do
