00109dbf91410b46df4faab089e65401e3fe09ee
Ali Jelveh <jelveh@web.de>
2010-09-06 17:05:33 +0200

    * add a little more info if something breaks ;) http://www.sqlite.org/draft/wal.html

--- db/migrate/20100906144216_move_sqlite_database_to_wal.rb
+++ db/migrate/20100906144216_move_sqlite_database_to_wal.rb
@@ -1,7 +1,7 @@
 class MoveSqliteDatabaseToWal < ActiveRecord::Migration
   def self.up
     result = ActiveRecord::Base.connection.execute('PRAGMA journal_mode = wal')
-    raise RuntimeError if RAILS_ENV == "production" && result[0][0] != 'wal'
+    raise(RuntimeError, "please install sqlite 3.7 or higher -> babushka dudemeister:sqlite3.7") if RAILS_ENV == "production" && result[0][0] != 'wal'
   end
 
   def self.down
