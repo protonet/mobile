11402cbd029e9c26fea76c0c3b53b8de0eefe9af
Ali Jelveh <jelveh@web.de>
2010-09-08 17:48:33 +0200

    environmentize js dispatcher

--- messaging/js_dispatching_control.rb
+++ messaging/js_dispatching_control.rb
@@ -4,8 +4,9 @@ require 'daemons'
 Daemons.run(
   File.join(File.dirname(__FILE__),'event_machine/js_dispatching_server.rb'),
   {
-    :dir_mode => :normal,
-    :dir => "tmp/pids",
+    :app_name   => "js_dispatcher_#{ENV["RAILS_ENV"]}",
+    :dir_mode   => :normal,
+    :dir        => "tmp/pids",
     :log_output => "log"
   }
 )
