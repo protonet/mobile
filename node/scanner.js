d9edd684b1d8e2b56505b756e240bf1d6fab16dc
Ali Jelveh <jelveh@web.de>
2010-09-08 17:56:27 +0200

    added bethaus scanner bridge

---
+++ node/scanner.js
@@ -0,0 +1,27 @@
+var sys = require("sys");
+var net = require("net");
+var fs  = require("fs");
+var http = require('http');
+var querystring = require('querystring');
+exec  = require('child_process').exec;
+
+fs.watchFile('/home/betahaus-scanner/scans', function (curr, prev) {
+  console.log('changed file is: ' + sys.inspect(curr));
+
+  child = exec('mv /home/betahaus-scanner/scans/* /home/protonet/dashboard/shared/user-files/9', 
+    function (error, stdout, stderr) {
+      sys.print('stdout: ' + stdout);
+      sys.print('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      } else {
+        exec('chown protonet:protonet -R /home/protonet/dashboard/shared/user-files/9');
+        setTimeout(function(){
+          var localNode = http.createClient(80, 'localhost');
+          var request = localNode.request('POST', '/tweets', {'host': 'localhost'});
+          request.write(querystring.stringify({"message_channel_id":9, "tweet": {"message": "your scan has arrived, please reload the file browser"}}));
+          request.end();
+        }, 15000)
+      }
+  });
+});
