3db4a54705115928543f85eb34a4e01e1e376220
Daniel Danopia <me@danopia.net>
2010-09-10 18:12:56 -0400

    Here's the rest of the services commit

--- app/models/system/services.rb
+++ app/models/system/services.rb
@@ -1,42 +1,97 @@
 module System
   class Services
-    # move service stuff from environment.rb to here
-    def self.js_dispatcher
-      @js_dispatcher ||= DaemonController.new(
-         :identifier    => 'js_dispatching_server',
-         :start_command => 'ruby messaging/js_dispatching_control.rb start',
-         :stop_command  => 'ruby messaging/js_dispatching_control.rb stop',
-         :ping_command  => lambda { TCPSocket.new('localhost', configatron.socket.port) },
-         :pid_file      => "tmp/pids/js_dispatcher_#{Rails.env}.pid",
-         :log_file      => "log/js_dispatcher_#{Rails.env}.output",
-         :timeout       => 45
-      )
+    def self.services; @services ||= {}; end
+    
+    def self.add label, entry, config
+      services[config[:identifier]] = [label, entry, DaemonController.new(config)]
     end
     
-    def self.solr
-      @solr_port ||= Sunspot.config.solr.url.match(/:(\d+)\//)[1]
-      @solr ||= DaemonController.new(
-         :identifier    => 'solr_server',
-         :start_command => "script/init/sunspot start -p #{@solr_port} -s solr --pid-dir=tmp/pids --pid-file=sunspot-solr-#{Rails.env}.pid -d solr/data/#{Rails.env}",
-         :stop_command  => "script/init/sunspot stop  -p #{@solr_port} -s solr --pid-dir=tmp/pids --pid-file=sunspot-solr-#{Rails.env}.pid -d solr/data/#{Rails.env}",
-         :ping_command  => lambda { TCPSocket.new('localhost', @solr_port) },
-         :pid_file      => "tmp/pids/sunspot-solr-#{Rails.env}.pid",
-         :log_file      => "log/sunspot-solr-#{Rails.env}.log",
-         :timeout       => 60
-      )
+    def self.start_all
+      puts '--------------------------'
+      puts 'Checking all subsystems...'
+      puts
+
+      colored_on  = "\e[1m\e[32m[ UP ]\e[0m"
+      colored_off = "\e[1m\e[31m[DOWN]\e[0m"
+
+      # checking the messaging bus
+      # TODO: Do something about this!
+      fprint 'RabbitMQ:           '
+      configatron.messaging_bus_active = System::MessagingBus.active?
+      puts configatron.messaging_bus_active ? colored_on : colored_off
+      
+      # checking dynamic services
+      services.each_value do |(name, entry, klass)|
+        fprint((name + ':').ljust(20))
+        
+        if klass.running?
+          configatron.__send__ "#{entry}=", true
+          puts colored_on
+        else
+          fprint "\e[sstarting..." # save cursor
+          
+          begin
+            klass.start
+            
+            running = klass.running?
+            configatron.__send__ "#{entry}=", running
+            puts "\e[u\e[K" + (running ? colored_on : colored_off) # restore & clear to end
+          rescue DaemonController::StartTimeout
+            configatron.__send__ "#{entry}=", false
+            puts "\e[u\e[K" + (colored_off) + " Failed to start in time"
+          end
+        end
+      end
+
+      # TODO: Do something about this!
+      configatron.ldap.active = false
+      puts "LDAP:               #{configatron.ldap.active ? colored_on : colored_off}"
+      puts '------------------------'
     end
-    
-    def self.node
-      @node ||= DaemonController.new(
-         :identifier    => 'node.js',
-         :start_command => 'node node/node.js',
-         :ping_command  => lambda { TCPSocket.new('localhost', 8124) },
-         :pid_file      => 'tmp/pids/node.pid',
-         :log_file      => 'log/node.log',
-         :timeout       => 25,
-         :daemonize_for_me => true
-      )
+
+    def self.stop_all
+      fprint "Shutting down services: \e[s" # save cursor
+      
+      services.each_value do |(name, entry, klass)|
+        fprint "\e[u\e[K#{name}" # restore & clear to end
+        
+        klass.stop if klass.running?
+      end
+      
+      fprint "\r\e[2K" # Move to left and clear line
     end
-    
+
+    def self.fprint *line # print and flush
+      $stdout.print *line
+      $stdout.flush
+    end
   end
 end
+
+System::Services.add 'Socket server', :js_dispatching_active,
+  :identifier    => 'js_dispatching_server',
+  :start_command => 'ruby messaging/js_dispatching_control.rb start',
+  :stop_command  => 'ruby messaging/js_dispatching_control.rb stop',
+  :ping_command  => lambda { TCPSocket.new('localhost', configatron.socket.port) },
+  :pid_file      => "tmp/pids/js_dispatcher_#{Rails.env}.pid",
+  :log_file      => "log/js_dispatcher_#{Rails.env}.output",
+  :timeout       => 45
+
+solr_port = Sunspot.config.solr.url.match(/:(\d+)\//)[1]
+System::Services.add 'Sunspot/Solr', :sunspot_active,
+  :identifier    => 'solr_server',
+  :start_command => "script/init/sunspot start -p #{solr_port} -s solr --pid-dir=tmp/pids --pid-file=sunspot-solr-#{Rails.env}.pid -d solr/data/#{Rails.env}",
+  :stop_command  => "script/init/sunspot stop  -p #{solr_port} -s solr --pid-dir=tmp/pids --pid-file=sunspot-solr-#{Rails.env}.pid -d solr/data/#{Rails.env}",
+  :ping_command  => lambda { TCPSocket.new('localhost', solr_port) },
+  :pid_file      => "tmp/pids/sunspot-solr-#{Rails.env}.pid",
+  :log_file      => "log/sunspot-solr-#{Rails.env}.log",
+  :timeout       => 60
+
+System::Services.add 'Node.JS', :nodejs_active,
+  :identifier    => 'node.js',
+  :start_command => 'node node/node.js',
+  :ping_command  => lambda { TCPSocket.new('localhost', 8124) },
+  :pid_file      => 'tmp/pids/node.pid',
+  :log_file      => 'log/node.log',
+  :timeout       => 25,
+  :daemonize_for_me => true
