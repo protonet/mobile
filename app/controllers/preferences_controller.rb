e2c70b25aec778674018432af7c51959f882be04
Daniel Danopia <me@danopia.net>
2010-09-15 15:17:23 -0400

    Added a node settings 'tab' to the preferences view

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,6 +3,7 @@ class PreferencesController < ApplicationController
   def index
     @preferences = [
       {:url => 'profile', :name => 'your profile'},
+      {:url => 'node_settings', :name => 'node settings'},
       {:url => 'network_settings', :name => 'network settings'},
       {:url => 'wifi_settings', :name => 'wifi settings'}, 
       {:url => 'system_overview', :name => 'system overview'}]
@@ -10,6 +11,20 @@ class PreferencesController < ApplicationController
   
   def profile
     render :partial => 'profile'
+  end
+  
+  def node_settings
+    @node = Network.local
+    
+    if request.put?
+      if @node.update_attributes(params[:network])
+        # TODO: send out the new info (via negotiating)
+        render :text => 'The local node information has been updated.'
+        return
+      end
+    end
+    
+    render :partial => 'node_settings'
   end
   
   def network_settings
