ca38f2bd13cd2cdff6a745969b9c73d8d645bac8
Ali Jelveh <jelveh@web.de>
2010-09-24 13:47:52 +0200

    * vpn controls

--- app/models/system/vpn.rb
+++ app/models/system/vpn.rb
@@ -3,15 +3,15 @@ module System
     
     class << self
       def start
-      
+        `/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current start #{Network.local.uuid} foobar`
       end
     
       def stop
-      
+        `/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current stop`
       end
     
       def status
-        "off"
+        system("/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current status")
       end
     end
 
