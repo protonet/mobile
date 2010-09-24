ecc80fdf5b28ab29daf9c30d7b5515fc124a99e7
Ali Jelveh <jelveh@web.de>
2010-09-24 18:06:54 +0200

    sudo is needed on those commands

--- app/models/system/vpn.rb
+++ app/models/system/vpn.rb
@@ -4,17 +4,17 @@ module System
     class << self
       def start
         return unless Rails.env == 'production'
-        `/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current start #{Network.local.uuid} foobar`
+        `sudo /home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current start #{Network.local.uuid} foobar`
       end
     
       def stop
         return unless Rails.env == 'production'
-        `/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current stop`
+        `sudo /home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current stop`
       end
     
       def status
         return 'off' unless Rails.env == 'production'
-        system("/home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current status") ? 'on' : 'off'
+        system("sudo /home/protonet/dashboard/current/script/init/vpn /home/protonet/dashboard/current status") ? 'on' : 'off'
       end
     end
 
