7c4c86c40e9d5ffd2518b1d5116ede4d566cb3cf
Ali Jelveh <jelveh@web.de>
2010-10-27 21:30:13 +0200

    another bugfix in the deployment

--- app/models/system/release.rb
+++ app/models/system/release.rb
@@ -26,7 +26,7 @@ module System
       def update!
         return false if Rails.env != 'production'
         if File.exist?("/home/protonet/deploy")
-          system("cd /home/protonet/deploy; /usr/bin/git pull; bin/deploy -r protonet -e production -m deploy -c #{configatron.shared_file_path}/protonet.d/deploy_config")
+          system("cd /home/protonet/deploy; /usr/bin/git pull; bin/deploy -r protonet -e production -m deploy -c #{configatron.shared_file_path}/config/protonet.d/deploy_config")
         end
       end
 
