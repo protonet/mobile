9393b1d11296666bbd389cc875750ab8271e80a5
Ali Jelveh <jelveh@web.de>
2010-10-27 15:24:30 +0200

    * more node update stuff

--- app/controllers/system/releases_controller.rb
+++ app/controllers/system/releases_controller.rb
@@ -2,6 +2,7 @@ module System
   class ReleasesController < ApplicationController
     
     def update
+      System::Release.update!
       render :text => 'bam!'
     end
     
--- app/models/system/release.rb
+++ app/models/system/release.rb
@@ -6,13 +6,25 @@ module System
       def current_version
         @current_version ||= File.readlines("#{RAILS_ROOT}/RELEASE")[0].to_i
       end
+      
+      def type
+        @type ||= File.readlines("#{RAILS_ROOT}/TYPE")[0]
+      end
 
       def latest_version
         Net::HTTP.get(URI.parse('http://releases.protonet.info/release/version')).to_i
       end
+      
+      def latest_beta_version
+        Net::HTTP.get(URI.parse('http://releases.protonet.info/beta/version')).to_i
+      end
 
       def most_recent?
         latest_version == current_version
+      end
+      
+      def update!
+        system("")
       end
 
     end
