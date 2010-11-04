b32064dc0610cfe5d2e279720ba07cc6254e711c
Ali Jelveh <jelveh@web.de>
2010-11-04 17:50:53 +0100

    * committing schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100927102645) do
+ActiveRecord::Schema.define(:version => 20101101213201) do
 
   create_table "channels", :force => true do |t|
     t.string   "name"
@@ -99,17 +99,17 @@ ActiveRecord::Schema.define(:version => 20100927102645) do
     t.string   "login",                          :limit => 40
     t.string   "name",                           :limit => 100, :default => ""
     t.string   "email",                          :limit => 100
-    t.string   "crypted_password",               :limit => 40
-    t.string   "salt",                           :limit => 40
+    t.string   "encrypted_password",             :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                                 :default => "",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token",                 :limit => 40
-    t.datetime "remember_token_expires_at"
     t.string   "communication_token"
     t.datetime "communication_token_expires_at"
     t.string   "temporary_identifier"
     t.boolean  "admin",                                         :default => false
     t.boolean  "guest",                                         :default => true
+    t.datetime "remember_created_at"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
