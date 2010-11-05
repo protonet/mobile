8a0d906c31a4ab2b37a0325e424fbf72f0c61905
Reza Jelveh <toyb0mb@gmx.net>
2010-11-05 09:42:03 +0100

    remove authenticatedtesthelper

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -121,7 +121,6 @@ end
 class UserAuthAndCreationTest < ActiveSupport::TestCase
   # Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead.
   # Then, you can remove it from this and the functional test.
-  include AuthenticatedTestHelper
   fixtures :users
 
   context "creating a user" do
