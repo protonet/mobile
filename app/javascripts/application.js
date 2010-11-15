830caf8c953b5707fde52a56acda83397ecd245f
Christopher Blum <christopher.blum@xing.com>
2010-11-15 20:22:44 +0100

    Update jquery to v1.4.4

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require "lib/jquery-1.4.3.min.js"
+//= require "lib/jquery-1.4.4.min.js"
 //= require "lib/jquery.jsonp-1.1.3.min.js"
 //= require "lib/json.min.js"
 //= require "lib/swfobject-2.2.js"
@@ -22,7 +22,7 @@ $(function() {
     var input = $(this);
     new protonet.utils.InlineHint(input, input.attr("title"));
   });
-  
+
   // add notification message neatification
   protonet.ui.FlashMessage.initialize();
   
