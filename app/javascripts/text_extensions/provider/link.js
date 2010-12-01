2ddd2ffc63507ac7ecdf3da4f0ae5bb729220298
Christopher Blum <christopher.blum@xing.com>
2010-12-01 22:46:24 +0100

    Make our text extensions compatible with grooveshark and ajax hash urls

--- app/javascripts/text_extensions/provider/link.js
+++ app/javascripts/text_extensions/provider/link.js
@@ -8,11 +8,15 @@
  */
 protonet.text_extensions.provider.Link = {
   REG_EXP: /https?:\/\/.{4,}/i,
+  REG_EXP_AJAX_PARAM: /\#\!?\//,
   
   loadData: function(url, onSuccess) {
     var queryUrl = protonet.utils.stripTrackingParams(url),
         urlParts = protonet.utils.parseUrl(queryUrl),
         shortUrl = urlParts.host + urlParts.path + urlParts.query;
+    
+    // Strip ajax hash (often used for crawlable ajax content)
+    queryUrl = queryUrl.replace(this.REG_EXP_AJAX_PARAM, "");
     
     var metaDataCallback = function(response) {
       response = response || {};
