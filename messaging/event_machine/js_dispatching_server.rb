e10d2227d2584ae1aff5954e0e2c8845a27f351d
Reza Jelveh <toyb0mb@gmx.net>
2010-12-08 21:41:23 +0100

    added http server to dispatcher

--- messaging/event_machine/js_dispatching_server.rb
+++ messaging/event_machine/js_dispatching_server.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'eventmachine'
+require 'evma_httpserver'
+
 # this is here to make sure environment.rb doens't recreate the EventMachine Loop
 RUN_FROM_DISPATCHER = true
 
@@ -8,6 +12,35 @@ require File.dirname(__FILE__) + '/../../config/environment'
 require File.dirname(__FILE__) + '/node_connection.rb'
 require File.dirname(__FILE__) + '/client_connection.rb'
 require File.dirname(__FILE__) + '/client_tracker.rb'
+
+class MyHttpServer < EM::Connection
+  include EM::HttpServer
+
+  # def post_init
+  #   super
+  #   no_environment_strings
+  # end
+
+  def process_http_request
+    # the http request details are available via the following instance variables:
+    #   @http_protocol
+    #   @http_request_method
+    #   @http_cookie
+    #   @http_if_none_match
+    #   @http_content_type
+    #   @http_path_info
+    #   @http_request_uri
+    #   @http_query_string
+    #   @http_post_content
+    #   @http_headers
+
+    response = EM::DelegatedHttpResponse.new(self)
+    response.status = 200
+    response.content_type 'text/html'
+    response.content = '<center><h1>Hi there</h1></center>'
+    response.send_response
+  end
+end 
 
 EventMachine::run do
   host = '0.0.0.0'
@@ -18,6 +51,8 @@ EventMachine::run do
   
   EventMachine.epoll if RUBY_PLATFORM =~ /linux/ #sky is the limit
   EventMachine::start_server host, port, ClientConnection, tracker
+  EventMachine::start_server host, 8000, MyHttpServer
+
   
   puts "Started socket server on #{host}:#{port}..."
   puts $$
