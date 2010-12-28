d9c4fb715b897eeac2b5e5920eb31db0c6c63dc3
Ali Jelveh <jelveh@web.de>
2010-12-28 16:07:23 +0100

    handling class reloads in our System::Preferences

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,9 @@
-System::Preferences.defaults[:wifi_mode] = :dual
-System::Preferences.defaults[:admin_key] = ActiveSupport::SecureRandom.base64(10)
-System::Preferences.defaults[:allow_dashboard_for_strangers] = true
-System::Preferences.defaults[:allow_registrations_for_strangers] = true
+require 'dispatcher'
+Dispatcher.to_prepare do
+
+  System::Preferences.defaults[:wifi_mode] = :dual
+  System::Preferences.defaults[:admin_key] = ActiveSupport::SecureRandom.base64(10)
+  System::Preferences.defaults[:allow_dashboard_for_strangers] = true
+  System::Preferences.defaults[:allow_registrations_for_strangers] = true
+
+end
