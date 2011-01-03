c0657090f660d64d65c5a0015c5de489a49c0e47
Ali Jelveh <jelveh@web.de>
2011-01-03 22:15:44 +0100

    * working towards our new update system

--- app/models/system/release.rb
+++ app/models/system/release.rb
@@ -25,10 +25,14 @@ module System
       
       def update!(password=nil)
         return false if Rails.env != 'production'
+        license_key = "FIXME"
         success = true
         if File.exist?("/home/protonet/deploy")
-          success =  success && system("/home/protonet/dashboard/current/script/init/update_release #{configatron.shared_file_path}")
-          success =  success && system("export HISTIGNORE=\"*ptn_babushka_migrations*\"; /home/protonet/dashboard/current/script/babushka_migrations #{password}")
+          success = success && system("export HISTIGNORE=\"*ptn_babushka_update*\"; /home/protonet/dashboard/current/script/ptn_babushka_update #{license_key}")
+          
+          success =  success && system("export HISTIGNORE=\"*ptn_babushka_migrations*\"; /home/protonet/dashboard/current/script/ptn_babushka_migrations #{password}")
+          
+          success =  success && system("/home/protonet/dashboard/current/script/ptn_update_release #{configatron.shared_file_path}")
         end
       end
 
