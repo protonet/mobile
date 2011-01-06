fa752d610571f89e6bbaa8b6a4a850a7d22bc79b
Christopher Blum <christopher.blum@xing.com>
2011-01-06 22:17:59 +0100

    Possibility to add hover menus to meeps

--- app/javascripts/behaviors/meeps.js
+++ app/javascripts/behaviors/meeps.js
@@ -1,3 +1,10 @@
 protonet.utils.Behaviors.add({
-  
+  ".meeps > li:hover": function(element, event) {
+    // works but is disabled for now
+    // if (event.type == "mouseenter") {
+    //   element.find(".author").append(' <a href="?meep=1" data-meep-id="1">link</span>');
+    // } else {
+    //   element.find(".author a[data-meep-id]").remove();
+    // }
+  }
 });
