9749d36254a45689ea5879511c15da402026e37b
Ali Jelveh <jelveh@web.de>
2011-01-18 18:28:38 +0100

    *  using 127.0.0.1 so it works on the integration server too

--- features/step_definitions/imageproxy_steps.rb
+++ features/step_definitions/imageproxy_steps.rb
@@ -7,22 +7,22 @@ Then /^proxying "([^"]*)" should work$/ do |url|
   # assert_equal content_type, open("http://127.0.0.1:#{configatron.nodejs.port}/image_proxy?url=#{url}").content_type
   #cleanup
   `rm #{RAILS_ROOT}/public/externals/image_proxy/#{Digest::MD5.hexdigest(url)} 2>&1 > /dev/null`
-  assert open("http://localhost:#{configatron.nodejs.port}/image_proxy?url=#{url}").content_type.match(/image/)
+  assert open("http://127.0.0.1:#{configatron.nodejs.port}/image_proxy?url=#{url}").content_type.match(/image/)
 end
 
 Then /^resizing "([^"]*)" should work$/ do |url|
 #cleanup
   `rm #{RAILS_ROOT}/public/externals/image_proxy/#{Digest::MD5.hexdigest(url)}_100_100 2>&1 > /dev/null`
-  assert open("http://localhost:#{configatron.nodejs.port}/image_proxy?height=100&width=100&url=#{url}").content_type.match(/image/)
+  assert open("http://127.0.0.1:#{configatron.nodejs.port}/image_proxy?height=100&width=100&url=#{url}").content_type.match(/image/)
 end
 
 Then /^screenshot resizing "([^"]*)" should work$/ do |url|
   #cleanup
-  screenshot = Digest::MD5.hexdigest("http://localhost:#{configatron.nodejs.port}/screenshooter?url=#{url}")
+  screenshot = Digest::MD5.hexdigest("http://127.0.0.1:#{configatron.nodejs.port}/screenshooter?url=#{url}")
   `rm #{RAILS_ROOT}/public/externals/image_proxy/#{screenshot} 2>&1 > /dev/null`
   `rm #{RAILS_ROOT}/public/externals/image_proxy/#{screenshot}* 2>&1 > /dev/null`
   `rm #{RAILS_ROOT}/public/externals/screenshots/#{Digest::MD5.hexdigest(url)}-clipped.png 2>&1 > /dev/null`
   
-  url = "http://localhost:#{configatron.nodejs.port}/image_proxy?height=100&width=100&url=http://localhost:#{configatron.nodejs.port}/screenshooter?url=#{url}"
+  url = "http://127.0.0.1:#{configatron.nodejs.port}/image_proxy?height=100&width=100&url=http://127.0.0.1:#{configatron.nodejs.port}/screenshooter?url=#{url}"
  assert open(url).content_type.match(/image/)
 end
