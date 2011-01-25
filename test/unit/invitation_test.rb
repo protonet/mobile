508f2e5ccdd50244dea25c0ab7e26adf19f225cc
dbloete <mail@dennisbloete.de>
2011-01-25 20:59:02 +0100

    Fixed invitation test

--- test/unit/invitation_test.rb
+++ test/unit/invitation_test.rb
@@ -1,12 +1,34 @@
 require 'test_helper'
 
-class InvitationTest < ActiveSupport::TestCase
-  
-  test "validation should create token" do
+class InvitationTest < Test::Unit::TestCase
+  before do
     @invitation = Invitation.new
-    assert @invitation.token.nil?
-    assert !@invitation.valid?
-    assert @invitation.token
+  end
+  
+  context "validation" do
+    
+    it "should ensure there is a token" do
+      @invitation.valid?
+      assert !@invitation.token.nil?
+    end
+    
+    it "should require at least one channel id" do
+      assert !@invitation.valid?
+      assert @invitation.errors.on(:channel_ids)
+    end
+    
+    it "should require a valid email address" do
+      @invitation.email = "test"
+      assert !@invitation.valid?
+      assert @invitation.errors.on(:email)
+      @invitation.email = "test@test"
+      assert !@invitation.valid?
+      assert @invitation.errors.on(:email)
+      @invitation.email = "test@test.com"
+      @invitation.valid?
+      assert !@invitation.errors.on(:email)
+    end
+    
   end
   
 end
