32b5da7fba91ff8bb83a01346ac951d4e9502628
Ali Jelveh <jelveh@web.de>
2011-01-31 21:22:20 +0100

    * schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110125101441) do
+ActiveRecord::Schema.define(:version => 20110131201908) do
 
   create_table "channels", :force => true do |t|
     t.string   "name"
