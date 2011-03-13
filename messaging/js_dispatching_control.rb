23abfd94011b1254d5fd0c472c2d46e7e4e2bd00
Ali Jelveh <jelveh@web.de>
2011-03-13 16:16:57 +0100

    * minor bugfixes

--- messaging/js_dispatching_control.rb
+++ messaging/js_dispatching_control.rb
@@ -5,7 +5,7 @@ require 'daemons'
 Daemons.run(
   File.join(File.dirname(__FILE__),'event_machine/js_dispatching_server.rb'),
   {
-    :app_name   => "js_dispatcher_#{ENV["Rails.env"]}",
+    :app_name   => "js_dispatcher_#{ENV["RAILS_ENV"]}",
     :dir_mode   => :normal,
     :dir        => "tmp/pids",
     :log_output => "log"
