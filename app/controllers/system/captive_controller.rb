2a6e1ba22423737fb8d3f79b50664c7e33e7c51a
Ali Jelveh <jelveh@web.de>
2011-03-24 20:59:35 +0100

    * fixing namespace

--- app/controllers/system/captive_controller.rb
+++ app/controllers/system/captive_controller.rb
@@ -1,22 +1,24 @@
-class SystemCaptiveController < ApplicationController
+module System
+  class CaptiveController < ApplicationController
   
-  def index
-  end
+    def index
+    end
   
-  # this works fine, but nicer would be to solve this in rack. So not Rails handles 404
-  def catchall
-    render :file => "#{Rails.root}/public/404.html", :status => 404
-  end
+    # this works fine, but nicer would be to solve this in rack. So not Rails handles 404
+    def catchall
+      render :file => "#{Rails.root}/public/404.html", :status => 404
+    end
   
-  def login
-    local_filename = 'tmp/captive_users'
-    doc = request.remote_ip + "\t" + SystemBackend.get_mac_for_ip(request.remote_ip) + "\t"  + Time.now().strftime("%d.%m.%y") + "\n"
+    def login
+      local_filename = 'tmp/captive_users'
+      doc = request.remote_ip + "\t" + SystemBackend.get_mac_for_ip(request.remote_ip) + "\t"  + Time.now().strftime("%d.%m.%y") + "\n"
     
-    File.open(local_filename, 'a') {|f| f.write(doc) }
+      File.open(local_filename, 'a') {|f| f.write(doc) }
     
-    SystemBackend.grant_internet_access(request.remote_ip)
-    sleep 3
-    redirect_to params[:req]
-  end
+      SystemBackend.grant_internet_access(request.remote_ip)
+      sleep 3
+      redirect_to params[:req]
+    end
 
+  end
 end
