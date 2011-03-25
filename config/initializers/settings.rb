73db46baa31b90a47e7409bfe4583ae4247892c0
Ali Jelveh <jelveh@web.de>
2011-03-25 18:29:18 +0100

    * sys preferences fixes

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,12 +1,10 @@
-# TODO: RAILS 3
-# require 'dispatcher'
-# Dispatcher.to_prepare do
-
+# preference defaults are stored in the class variables of the systempreferences object
+# since rails reloads those models we'll need to reset these on every request
+Dashboard::Application.config.to_prepare do
   SystemPreferences.defaults[:wifi_mode] = :dual
   SystemPreferences.defaults[:admin_key] = ActiveSupport::SecureRandom.base64(10)
   SystemPreferences.defaults[:allow_dashboard_for_strangers] = true
   SystemPreferences.defaults[:allow_registrations_for_strangers] = true
   SystemPreferences.defaults[:public_host] = "localhost:3000"
   SystemPreferences.defaults[:public_host_https]  = false
-  
-# end
+end
