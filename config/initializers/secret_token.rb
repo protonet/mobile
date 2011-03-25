a4e8f5e5bfde872ed6b41fd763af1a61382b8b85
Ali Jelveh <jelveh@web.de>
2011-03-25 18:29:05 +0100

    * session settings stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dashboard::Application.config.secret_token = '48a0979d9654a895f7fce47d1ddda8a23b6c2314835b509e3f5daaaa18e6ef1661829ea4b0d4d9f88022b24cb7f5a5513bc3669f75c163dde1df66160f4ff3df'
+Dashboard::Application.config.secret_token = ((System::Preferences.session_secret ||= ActiveSupport::SecureRandom.base64(120)) rescue ActiveSupport::SecureRandom.base64(120))
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Dashboard::Application.config.session_store :cookie_store, :key => '_rails_dashboard_session'
-Dashboard::Application.config.secret_token = ((System::Preferences.session_secret ||= ActiveSupport::SecureRandom.base64(120)) rescue ActiveSupport::SecureRandom.base64(120))
 
 # ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, FlashSessionCookieMiddleware, ActionController::Base.session_options[:key])
