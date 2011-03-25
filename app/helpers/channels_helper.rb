b8fae25ab7657e245a21446710b43f4a9fc7a571
Ali Jelveh <jelveh@web.de>
2011-03-25 18:26:12 +0100

    * add raw so the link is coreclty shown

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -2,9 +2,9 @@ module ChannelsHelper
 
   def channel_listen_control(channel)
     if current_user.channels.include?(channel)
-      link_to('<p id="channel-listener"> stop listening </p>', current_user.listens.find_by_channel_id(channel.id), :class => 'listen off', :method => :delete)
+      link_to(raw('<p id="channel-listener"> stop listening </p>'), current_user.listens.find_by_channel_id(channel.id), :class => 'listen off', :method => :delete)
     else
-      link_to('<p id="channel-listener"> start listening </p>', listens_path(:channel_id => channel.id), :class => 'listen on', :method => :post)
+      link_to(raw('<p id="channel-listener"> start listening </p>'), listens_path(:channel_id => channel.id), :class => 'listen on', :method => :post)
     end
   end
   
