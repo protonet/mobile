61f66832bc058d1119585fd198f235c42218b2dc
Christopher Blum <christopher.blum@xing.com>
2011-03-30 17:46:10 +0200

    Detect other chrome flash blocker and ignore flash blocker placeholder for swfobject dummy elements

--- app/javascripts/ui/click_to_flash_teaser.js
+++ app/javascripts/ui/click_to_flash_teaser.js
@@ -1,43 +1,67 @@
 // Check for flash placeholder
 $(window).load(function() {
+  var selector = "div.ujs_flashblock_placeholder, div[bginactive][bgactive], div[style*='gofhjkjmkpinhpoiabjplobcaignabnl']";
+  
+  // setTimout needed, since some flash blocker take a bit longer than window.onload to init
   setTimeout(function() {
-    var flashPlaceholder = $("div.ujs_flashblock_placeholder, div[bginactive][bgactive]");
-    flashPlaceholder.css({
-      "border":             "5px solid white",
-      "box-sizing":         "content-box",
-      "-moz-box-sizing":    "content-box",
-      "-webkit-box-sizing": "content-box",
-      "width":              "auto",
-      "height":             "auto",
-      "min-width":          "90px",
-      "min-height":         "90px",
-      "margin-left":        "-50px",
-      "position":           "absolute",
-      "left":               "50%",
-      "top":                "55px",
-      "box-shadow":         "0 0 30px #000",
-      "-moz-box-shadow":    "0 0 30px #000",
-      "-webkit-box-shadow": "0 0 30px #000",
-      "z-index":            "100"
-    }).bind("DOMNodeRemoved", function() {
+    var flashPlaceholder = $(selector).map(function() {
+      var element = this;
+      if (this.style.cssText.indexOf("gofhjkjmkpinhpoiabjplobcaignabnl") != -1) {
+        element = element.parentNode;
+      }
+      // Filter flash block placeholder that replace swfobject dummy elements
+      var nextSibling                 = element.nextSibling || {},
+          nextSiblingIsSwfObjectDummy = nextSibling.nodeName == "OBJECT" && !nextSibling.data;
+      if (nextSiblingIsSwfObjectDummy) {
+        element.parentNode.removeChild(element);
+        return null;
+      }
+      return element;
+    });
+    
+    if (!flashPlaceholder.length) {
+      return;
+    }
+    
+    var interval = setInterval(function() {
+      flashPlaceholder.css({
+        "border":             "5px solid white",
+        "box-sizing":         "content-box",
+        "-moz-box-sizing":    "content-box",
+        "-webkit-box-sizing": "content-box",
+        "width":              "90px",
+        "height":             "90px",
+        "min-width":          "90px",
+        "min-height":         "90px",
+        "margin-left":        "-50px",
+        "position":           "absolute",
+        "left":               "50%",
+        "top":                "55px",
+        "box-shadow":         "0 0 30px #000",
+        "-moz-box-shadow":    "0 0 30px #000",
+        "-webkit-box-shadow": "0 0 30px #000",
+        "z-index":            "100"
+      });
+    }, 50);
+    
+    flashPlaceholder.bind("DOMNodeRemoved", function() {
       setTimeout(function() {
-        flashPlaceholder = $("div.ujs_flashblock_placeholder, div[bginactive][bgactive]");
+        flashPlaceholder = $(selector);
         if (!flashPlaceholder.length) {
+          clearInterval(interval);
           overlay.remove();
         }
       }, 0);
     });
-
-    if (flashPlaceholder.length) {
-      var overlay = $("<div>").css({
-        "background":       "rgba(255, 255, 255, 0.7) url(/img/click_to_flash_teaser.png) center 10px no-repeat ", 
-        "z-index":          "99",
-        "position":         "absolute",
-        "top":              0,
-        "left":             0,
-        "width":            "100%",
-        "height":           "100%"
-      }).appendTo("body");
-    }
+    
+    var overlay = $("<div>").css({
+      "background": "rgba(255, 255, 255, 0.7) url(/img/click_to_flash_teaser.png) center 10px no-repeat ", 
+      "z-index":    "99",
+      "position":   "absolute",
+      "top":        0,
+      "left":       0,
+      "width":      "100%",
+      "height":     "100%"
+    }).appendTo("body");
   }, 100);
 });
