dfb6bed7a8d0db4b978632116c5563ad8d91defe
Ali Jelveh <jelveh@web.de>
2011-04-15 16:23:56 +0200

    update system preferences to work with rails 3

--- app/models/system_preferences.rb
+++ app/models/system_preferences.rb
@@ -1,31 +1,30 @@
-# from http://github.com/ledermann/rails-settings
 class SystemPreferences < ActiveRecord::Base
-  class PreferencesNotFound < RuntimeError; end
+  class SystemPreferenceNotFound < RuntimeError; end
 
-  set_table_name :system_preferences
-
+  set_table_name 'system_preferences'
+  
   cattr_accessor :defaults
   @@defaults = {}.with_indifferent_access
-
+  
   #get or set a variable with the variable as the called method
   def self.method_missing(method, *args)
     method_name = method.to_s
     super(method, *args)
-  
+    
   rescue NoMethodError
     #set a value for a variable
     if method_name =~ /=$/
       var_name = method_name.gsub('=', '')
       value = args.first
       self[var_name] = value
-  
+    
     #retrieve a value
     else
       self[method_name]
-    
+      
     end
   end
-
+  
   #destroy the specified settings record
   def self.destroy(var_name)
     var_name = var_name.to_s
@@ -33,22 +32,22 @@ class SystemPreferences < ActiveRecord::Base
       object(var_name).destroy
       true
     else
-      raise PreferencesNotFound, "Setting variable \"#{var_name}\" not found"
+      raise SystemPreferenceNotFound, "Setting variable \"#{var_name}\" not found"
     end
   end
 
   #retrieve all settings as a hash (optionally starting with a given namespace)
   def self.all(starting_with=nil)
     options = starting_with ? { :conditions => "var LIKE '#{starting_with}%'"} : {}
-    vars = object_scoped.all({:select => 'var, value'}.merge(options))
-  
+    vars = thing_scoped.find(:all, {:select => 'var, value'}.merge(options))
+    
     result = {}
     vars.each do |record|
       result[record.var] = record.value
     end
     result.with_indifferent_access
   end
-
+  
   #get a setting value by [] notation
   def self.[](var_name)
     if var = object(var_name)
@@ -59,73 +58,59 @@ class SystemPreferences < ActiveRecord::Base
       nil
     end
   end
-
+  
   #set a setting value by [] notation
   def self.[]=(var_name, value)
     var_name = var_name.to_s
-  
-    record = object(var_name) || object_scoped.new(:var => var_name)
+    
+    record = object(var_name) || thing_scoped.new(:var => var_name)
     record.value = value
     record.save!
-  
+    
     value
   end
-
+  
   def self.merge!(var_name, hash_value)
     raise ArgumentError unless hash_value.is_a?(Hash)
-  
+    
     old_value = self[var_name] || {}
     raise TypeError, "Existing value is not a hash, can't merge!" unless old_value.is_a?(Hash)
-  
+    
     new_value = old_value.merge(hash_value)
     self[var_name] = new_value if new_value != old_value
-  
+    
     new_value
   end
 
   def self.object(var_name)
-    object_scoped.find_by_var(var_name.to_s)
+    thing_scoped.find_by_var(var_name.to_s)
   end
-
+  
   #get the value field, YAML decoded
   def value
     YAML::load(self[:value])
   end
-
+  
   #set the value field, YAML encoded
   def value=(new_value)
     self[:value] = new_value.to_yaml
   end
-
-  def self.object_scoped
-    SystemPreferences.scoped_by_object_type_and_object_id(object_type, object_id)
+  
+  def self.thing_scoped
+    self.scoped_by_object_type_and_object_id(nil, nil)
   end
-
-  #Deprecated!
-  def self.reload # :nodoc:
-    self
-  end
-
-  def self.object_id
-    nil
-  end
-
-  def self.object_type
-    nil
-  end
+  
+  
 end
 
-class ScopedPreferences < SystemPreferences
-  def self.for_object(object)
+class ScopedSystemPreferences < SystemPreferences
+  def self.for_thing(object)
     @object = object
     self
   end
-
-  def self.object_id
-    @object.id
+  
+  def self.thing_scoped
+    SystemPreferences.scoped_by_object_type_and_object_id(@object.class.base_class.to_s, @object.id)
   end
 
-  def self.object_type
-    @object.class.base_class.to_s
-  end
 end
