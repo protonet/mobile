3ff6ddc015bc576177c30649c46854abc6d6dcb9
Ali Jelveh <jelveh@web.de>
2011-04-28 18:57:34 +0200

    first batch of captive portal work

---
+++ app/models/system_captive_portal.rb
@@ -0,0 +1,41 @@
+class SystemNetworking
+  
+  class << self
+    
+    def activate
+      `#{service_command} start`
+    end
+    
+    def deactivate
+      `#{service_command} flush`
+    end
+    
+    def iptables_rules_ready?
+      return false unless Rails.env == 'production'
+      return true if File.exists?("#{configatron.shared_file_path}/config/ifconfig.d/iptables")
+      !!FileUtils.cp("#{Rails.root}/lib/backend_adapters/ubuntu/iptables/protonet", "#{configatron.shared_file_path}/config/ifconfig.d/iptables")
+    end
+    
+    def captive_status
+      false
+    end
+    
+    private
+    def monitor_service
+      return if SystemMonit.exists?(:captive_portal) && SystemMonit.start(:captive_portal)
+      iptables_rules_ready?
+      start = "#{service_command} start"
+      stop  = "#{service_command} stop"
+      pid_file = "#{configatron.current_file_path}/tmp/pids/captive_portal.pid"
+      SystemMonit.add(:publish_to_web, start, stop, pid_file)
+    end
+    
+    def service_command
+      public_interface = "foo"
+      external_interface = "bar"
+      "/usr/bin/sudo #{configatron.current_file_path}/script/init/captive_portal #{configatron.current_file_path} #{public_interface} #{external_interface}"
+    end
+    
+  end
+
+end
--- app/models/system_networking.rb
+++ app/models/system_networking.rb
@@ -2,7 +2,7 @@ class SystemNetworking
   
   class << self
     
-    def iptables_rules_ready
+    def iptables_rules_ready?
       return false unless Rails.env == 'production'
       return true if File.exists?("#{configatron.shared_file_path}/config/ifconfig.d/iptables")
       !!FileUtils.cp("#{Rails.root}/lib/backend_adapters/ubuntu/iptables/protonet", "#{configatron.shared_file_path}/config/ifconfig.d/iptables")
@@ -11,12 +11,12 @@ class SystemNetworking
     def config_wifi_interface(interface, ip)
       #doesn't work
     end
-    
+
     def restart_interface(interface)
       `/sbin/ifconfig #{interface} down`
       `/sbin/ifconfig #{interface} up`
     end
-    
+
   end
 
 end
