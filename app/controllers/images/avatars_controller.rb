e10be10faea23ed8aee976a6fddec5f6f783a849
Ali Jelveh <jelveh@web.de>
2011-05-10 16:19:46 +0200

    * show validation errors

--- app/controllers/images/avatars_controller.rb
+++ app/controllers/images/avatars_controller.rb
@@ -5,7 +5,7 @@ class Images::AvatarsController < ApplicationController
     if current_user.save
       redirect_to preferences_path
     else
-      flash[:notice] = 'Your photo did not pass validation!'
+      flash[:notice] = "Your photo did not pass validation! #{current_user.errors.full_messages.to_sentence}"
       redirect_to :back
     end
   end
