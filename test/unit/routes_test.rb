4b65c7a38c16d1791e9efc1fc9844b99486cc157
Ali Jelveh <jelveh@web.de>
2011-05-12 21:59:47 +0200

    fixing route tests after changes

--- test/unit/routes_test.rb
+++ test/unit/routes_test.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class RoutingTest < ActionController::TestCase
 
   test "should correctly route vpn on off stuff" do
-    opts = {:controller => "system/vpn", :action => "on"}
-    assert_routing "system/vpn/on", opts
-    opts = {:controller => "system/vpn", :action => "off"}
-    assert_routing "system/vpn/off", opts
+    opts = {:controller => "preferences/vpn", :action => "on"}
+    assert_routing "preferences/vpn/on", opts
+    opts = {:controller => "preferences/vpn", :action => "off"}
+    assert_routing "preferences/vpn/off", opts
   end
   
   test "should correctly route file directory creation" do
