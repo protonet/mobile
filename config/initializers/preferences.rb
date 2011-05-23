641d46e1848c341fdbd8d567f77eb9aedcb11f5c
Ali Jelveh <jelveh@web.de>
2011-05-23 20:25:20 +0200

    * another batch of work for the whitelabel version

--- config/initializers/preferences.rb
+++ config/initializers/preferences.rb
@@ -37,5 +37,11 @@ Dashboard::Application.config.to_prepare do
   SystemPreferences.defaults[:public_host] = "localhost:3000"
   SystemPreferences.defaults[:public_host_https]  = false
   SystemPreferences.defaults[:captive_portal_greeting] = "Das ist das Captive Portal, Hallo!"
+  SystemPreferences.defaults[:browser_title] = "protonet - it's yours"
   SystemPreferences.defaults[:show_user_navigation] = true
+  SystemPreferences.defaults[:show_clouds] = true
+  SystemPreferences.defaults[:show_search_widget] = true
+  SystemPreferences.defaults[:show_user_widget] = true
+  SystemPreferences.defaults[:show_file_widget] = true
+  SystemPreferences.defaults[:show_only_online_users] = false
 end
