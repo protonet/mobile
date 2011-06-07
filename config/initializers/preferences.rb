dc491e8fc3fbcf3e9270bb0316b7de40c609dfa7
Ali Jelveh <jelveh@web.de>
2011-06-07 13:07:35 +0200

    set default publish to web name

--- config/initializers/preferences.rb
+++ config/initializers/preferences.rb
@@ -52,5 +52,5 @@ Dashboard::Application.config.to_prepare do
   SystemPreferences.defaults[:default_stranger_user_group] = "guest"
   SystemPreferences.defaults[:allow_modal_views] = true
   SystemPreferences.defaults[:custom_css_type] = "append"
-
+  SystemPreferences.defaults[:publish_to_web_name] = SystemPreferences.node_name
 end
