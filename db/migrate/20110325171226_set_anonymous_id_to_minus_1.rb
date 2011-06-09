7674f0c2b3cc4ac8a3d41931daed77184a912742
Christopher Blum <christopher.blum@xing.com>
2011-06-09 20:58:22 +0200

    Update migrations (@dudemeister did)

--- db/migrate/20110325171226_set_anonymous_id_to_minus_1.rb
+++ db/migrate/20110325171226_set_anonymous_id_to_minus_1.rb
@@ -1,9 +1,15 @@
 class SetAnonymousIdToMinus1 < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.connection.execute("update users set id = -1 where id = 0")
+    Tweet.update_all("user_id = -1", "user_id = 0")
+    Channel.update_all("owner_id = -1", "owner_id = 0")
+    Listen.update_all("user_id = -1", "user_id = 0")
   end
 
   def self.down
     ActiveRecord::Base.connection.execute("update users set id = 0 where id = -1")
+    Tweet.update_all("user_id = 0", "user_id = -1")
+    Channel.update_all("owner_id = 0", "owner_id = -1")
+    Listen.update_all("user_id = 0", "user_id = -1")
   end
 end
