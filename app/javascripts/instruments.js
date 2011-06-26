054ebeaed8171fefc1b864a7e06b087c595658b0
Christopher Blum <christopher.blum@xing.com>
2011-06-26 19:14:44 +0200

    Rollback accidental change in instruments.js and removed empty/unused css class

--- app/javascripts/instruments.js
+++ app/javascripts/instruments.js
@@ -17,11 +17,13 @@ $(function() {
   protonet.dispatcher.initialize();
   protonet.timeline.initialize();
   
-  protonet.globals.pages = {
-    search: new protonet.pages.Search(),
-    meep:   new protonet.pages.Meep(),
-    user:   new protonet.Page("users")
-  };
+  if (protonet.config.allow_modal_views) {
+    protonet.globals.pages = {
+      search: new protonet.pages.Search(),
+      meep:   new protonet.pages.Meep(),
+      user:   new protonet.Page("users") 
+    };
+  }
   
   // Init widgets
   if (protonet.config.show_user_widget) {
