1e26b22fd706017d4e2d8a6948d497ab28356c38
Christopher Blum <christopher.blum@xing.com>
2011-07-06 22:53:01 +0200

    Improved performance of textarea auto resizer

--- app/javascripts/timeline/form/extensions/auto_resizer.js
+++ app/javascripts/timeline/form/extensions/auto_resizer.js
@@ -1,35 +1,66 @@
 /**
  * Auto resizing for the textarea
  * Automatically increases height of textarea when text overflows
+ * Designed for optimal performance
  */
 protonet.timeline.Form.extensions.AutoResizer = function(input, wrapper) {
   if (!protonet.user.Browser.SUPPORTS_EVENT("input")) {
     return;
   }
   
-  var inputElement    = input[0],
-      wrapperElement  = wrapper[0],
-      originalHeight  = inputElement.offsetHeight,
-      offset          = 10;
+  var inputElement      = input[0],
+      wrapperElement    = wrapper[0],
+      originalHeight    = inputElement.offsetHeight,
+      oldHeight         = originalHeight,
+      offset            = 10,
+      scrollHeight,
+      cloneElement      = (function() {
+        // Properties which may effect space taken up by chracters:
+        var props = ["height", "width", "lineHeight", "textDecoration", "letterSpacing", "fontSize", "fontFamily"],
+            propOb = {};
+
+        // Create object of styles to apply:
+        $.each(props, function(i, prop) {
+          propOb[prop] = input.css(prop);
+        });
+        
+        // Clone the actual textarea removing unique properties
+        // and insert before original textarea:
+        var clone = input.clone().removeAttr("id").removeAttr("name").css({
+            position: "absolute",
+            top:      0,
+            left:     "-9999px"
+        }).css(propOb).attr("tabIndex", "-1").insertBefore(input);
+        
+        return clone[0];
+      })();
   
-  function updateHeight() {
-    inputElement.style.height = originalHeight.px();
-    var scrollHeight = inputElement.scrollHeight;
-    inputElement.style.height = "";
+  function updateHeight(useEffect) {
+    cloneElement.value = inputElement.value;
+    var scrollHeight = cloneElement.scrollHeight;
     
     if (scrollHeight <= originalHeight) {
-      wrapperElement.style.height = originalHeight.px();
+      newHeight = originalHeight;
     } else {
-      wrapperElement.style.height = (scrollHeight + offset).px();
+      newHeight = scrollHeight + offset;
+    }
+    
+    if (newHeight !== oldHeight) {
+      if (useEffect === true) {
+        wrapper.animate({ height: newHeight.px() }, 250);
+      } else {
+        wrapperElement.style.height = newHeight.px();
+      }
+      oldHeight = newHeight;
     }
   };
   
   input
     .bind("input", updateHeight)
-    .css("overflow", "hidden");
+    .css("overflow-y", "hidden");
   
   protonet.bind("form.submitted", function() {
     // delay for performance reasons
-    setTimeout(updateHeight, 100);
+    setTimeout(function() { updateHeight(true); }, 100);
   });
 };
