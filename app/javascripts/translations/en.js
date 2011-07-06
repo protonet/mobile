d2312c98be76701b96aaa69927a8fc2a29160498
Christopher Blum <christopher.blum@xing.com>
2011-07-07 00:23:51 +0200

    Bunch of minor layout and text fixes for white-label compatibility

--- app/javascripts/translations/en.js
+++ app/javascripts/translations/en.js
@@ -2,7 +2,7 @@ protonet.translations = {
   MEEP_SENT: "[sent!]",
   MEEP_ERROR: "[error!]",
   MEEP_SENDING: "[sending...]",
-  MEEP_ERROR_LONG: "Olala... your meep hasn't been sent. Try again or leave the computer and enjoy the wonderful sun outside!",
+  MEEP_ERROR_LONG: "Olala... your message hasn't been sent. Try again or leave the computer and enjoy the wonderful sun outside!",
   NO_MEEPS_AVAILABLE: "Hurry! Be the first one to post a message here.",
   REPLY_NOTIFICATION_TITLE: "protonet - new reply",
   CHANNEL_SUBSCRIPTION_ERROR: "could not subscribe to channel with identifier '{identifier}'",
