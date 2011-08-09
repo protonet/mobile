1b084c82fb2696e79b8d2c6d7ca49aee31ba7f30
Ali Jelveh <jelveh@web.de>
2011-08-09 13:26:20 +0200

    * add index to meeps on channel_id

--- db/migrate/20110809104622_kill_say_model.rb
+++ db/migrate/20110809104622_kill_say_model.rb
@@ -3,9 +3,11 @@ class KillSayModel < ActiveRecord::Migration
     add_column :meeps, :channel_id, :integer
     ActiveRecord::Base.connection.execute("update meeps left join says on says.meep_id = meeps.id set meeps.channel_id = says.channel_id")
     drop_table :says
+    add_index :meeps, :channel_id
   end
 
   def self.down
+    remove_index :meeps, :channel_id
     create_table :says, :force => true do |t|
       t.channel_id
       t.meep_id
