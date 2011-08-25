0814eddcd19a32f9eb85c90096ae8703735e10bb
Ali Jelveh <jelveh@web.de>
2011-08-25 17:14:53 +0200

    * handle channels with no meeps (just created ones)

--- app/models/listen.rb
+++ app/models/listen.rb
@@ -7,14 +7,9 @@ class Listen < ActiveRecord::Base
   scope :verified, :conditions => {:verified => true}
   
   # if a channel is public we set the verified status to public
-  after_create  :auto_set_verification
+  after_create  :auto_set_verification,:set_last_read_meep
   after_create  :send_subscribe_notification, :if => lambda {|listen| listen.verified?}
   after_destroy :send_unsubscribe_notification
-
-  def auto_set_verification
-    self.verified = true if channel.public? || channel.owned_by(user) || channel.rendezvous_participant?(user)
-    save
-  end
   
   def send_subscribe_notification
     send_channel_notification(:subscribed_channel)
@@ -42,4 +37,15 @@ class Listen < ActiveRecord::Base
     
     self.connection.execute(sql_query)
   end
+  
+  def set_last_read_meep
+    self.last_read_meep = channel.meeps.last.id rescue 0
+    save
+  end
+  
+  def auto_set_verification
+    self.verified = true if channel.public? || channel.owned_by(user) || channel.rendezvous_participant?(user)
+    save
+  end
+  
 end
