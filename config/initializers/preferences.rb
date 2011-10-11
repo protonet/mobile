cda25abcdb079ddc7a20cf86f19e02c9081e1f56
Christopher Blum <christopher.blum@xing.com>
2011-10-11 17:40:23 +0200

    Make hostapd more verbose and fix multiple bssid

--- config/initializers/preferences.rb
+++ config/initializers/preferences.rb
@@ -23,7 +23,7 @@ Dashboard::Application.config.to_prepare do
   }
   SystemPreferences.defaults[:wifi] = {
     "mode" => :dual,
-    "channel" => 3,
+    "channel" => 5, # should be 5 - 11 since 1 - 4 don't support multiple ssids
     "wlan0" => {
       "name" => "protonet-private",
       "password" => ActiveSupport::SecureRandom.base64(10),
