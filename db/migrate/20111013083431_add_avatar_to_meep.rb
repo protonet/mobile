f6aa55769d0ae34bbaec49f21a99521ed0206f56
Ali Jelveh <jelveh@web.de>
2011-10-17 13:56:39 +0200

    * this should work now, migration to meeps with avatar urls

--- db/migrate/20111013083431_add_avatar_to_meep.rb
+++ db/migrate/20111013083431_add_avatar_to_meep.rb
@@ -1,7 +1,7 @@
 class AddAvatarToMeep < ActiveRecord::Migration
   def self.up
     add_column :meeps, :avatar, :string
-    ActiveRecord::Base.connection.execute('update meeps, users set meeps.avatar = CONCAT("/system/avatars/", users.id, "/original/", users.avatar_file_name) where meeps.user_id = users.id')
+    ActiveRecord::Base.connection.execute('update meeps, users set meeps.avatar = CONCAT("/system/avatars/", users.id, "/original/", users.avatar_file_name, IF((SELECT users.avatar_file_name REGEXP ".*\\.") = 1, "", ".")) where meeps.user_id = users.id')
     ActiveRecord::Base.connection.execute("UPDATE meeps SET meeps.avatar = '/img/user_picture_r2.png' WHERE avatar IS NULL")
   end
 
