5634da00fe9a732378154207dad13ec5a9f5ac33
Christopher Blum <christopher.blum@xing.com>
2011-11-21 12:54:13 +0100

    Fix typo

--- app/javascripts/pages/preferences.js
+++ app/javascripts/pages/preferences.js
@@ -22,7 +22,7 @@ $(function() {
     return false;
   });
   
-  $page.delegate("form.wifi form.publish-to-web", "ajax:complete", function() {
+  $page.delegate("form.wifi, form.publish-to-web", "ajax:complete", function() {
     setTimeout(function() { $(this).find(".reload-link").click(); }.bind(this), 500);
   });
   
