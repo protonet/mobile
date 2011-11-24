a88f2de3e35b109d89362af0df11a78638a770ee
Christopher Blum <christopher.blum@xing.com>
2011-11-24 11:45:22 +0100

    Fix protonet.utils.isSameOrigin in IE browsers

--- app/javascripts/utils/is_same_origin.js
+++ app/javascripts/utils/is_same_origin.js
@@ -1,9 +1,12 @@
 protonet.utils.isSameOrigin = (function() {
-  var anchor    = document.createElement("a"),
-      location  = window.location;
+  var anchor        = document.createElement("a"),
+      location      = window.location,
+      PORT_REG_EXP  = /\:\d+$/;
   return function(url) {
     anchor.href = url;
+    var anchorHost    = anchor.host.replace(PORT_REG_EXP, ""),
+        locationHost  = location.host.replace(PORT_REG_EXP, "");
     return  anchor.protocol === location.protocol &&
-            anchor.host     === location.host;
+            anchorHost      === locationHost;
   };
 })();
