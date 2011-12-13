40b9913e027ec8e8d989994c779d448c9c93e426
Ali Jelveh <jelveh@web.de>
2011-12-13 10:58:36 +0100

    * autoupdate hostname after changes to it, fixes localhost:3000 bug until restart in invitations

--- app/models/system_preferences_observer.rb
+++ app/models/system_preferences_observer.rb
@@ -5,7 +5,8 @@ class SystemPreferencesObserver < ActiveRecord::Observer
       Network.update_local_from_preferences
     when "custom_css", "custom_javascript", "browser_title"
       system_preference.destroy if system_preference.value.blank?
-    when "public_host_https"
+    when "public_host", "public_host_https"
+      ActionMailer::Base.default_url_options[:host]     = SystemPreferences.public_host
       ActionMailer::Base.default_url_options[:protocol] = (SystemPreferences.public_host_https ? 'https' : 'http')
     when "index_meeps"
       Sunspot::IndexQueue::Entry.implementation =  (system_preference.value ? :active_record : :nil)
