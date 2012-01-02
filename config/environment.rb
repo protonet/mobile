d0c6346a38f99e0f8f27affcf8520ab3312e61aa
Daniel Lamando <me@danopia.net>
2012-01-02 11:55:50 +0100

    Silence a warning about RAILS_ENV from LDAP.
    The external lib checks for LDAP_ENV, then fails back to RAILS_ENV.
    Silenced depreciation warning by setting LDAP_ENV to Rails.env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Silence warning
+LDAP_ENV = Rails.env
+
 # Initialize the rails application
 Dashboard::Application.initialize!
