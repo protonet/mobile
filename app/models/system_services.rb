9ba6229cb6690a40e0cc28b7500a9185f56f4de1
Daniel Lamando <me@danopia.net>
2012-01-04 17:06:52 +0100

    Tweak the start/shutdown daemon script.
    More tags for glancing at, and turns off ANSI escape codes if not run
    through a TTY (i.e., for logfiles)

--- app/models/system_services.rb
+++ app/models/system_services.rb
@@ -1,63 +1,79 @@
 class SystemServices
   def self.services; @services ||= {}; end
-  
+
   def self.add label, entry, config
     services[config[:identifier]] = [label, entry, DaemonController.new(config)]
   end
-  
+
   def self.start_all
+    tty = $stdout.tty?
+
     puts '--------------------------'
     puts 'Checking all subsystems...'
     puts
 
-    colored_on  = "\e[1m\e[32m[ UP ]\e[0m"
-    colored_off = "\e[1m\e[31m[DOWN]\e[0m"
+    tags = {
+      true  => (tty ? "\e[1m\e[32m[ UP ]\e[0m" : '[ UP ]'),
+      :fail => (tty ? "\e[1m\e[31m[FAIL]\e[0m" : '[FAIL]'),
+      :time => (tty ? "\e[1m\e[31m[TIME]\e[0m" : '[TIME]'),
+      :down => (tty ? "\e[1m\e[33m[DOWN]\e[0m" : '[DOWN]'),
+      :wait => (tty ? "\e[1m\e[30m[WAIT]\e[0m" : '[WAIT]'),
+    }
 
     # checking the messaging bus
-    # TODO: Do something about this!
-    fprint 'RabbitMQ:           '
+    fprint(('RabbitMQ:').ljust(20))
     configatron.messaging_bus_active = SystemMessagingBus.active?
-    puts configatron.messaging_bus_active ? colored_on : colored_off
-    
+    puts tags[configatron.messaging_bus_active || :down]
+
     # checking dynamic services
     services.each_value do |(name, entry, klass)|
       fprint((name + ':').ljust(20))
-      
+
       if klass.running?
         configatron.__send__ "#{entry}=", true
-        puts colored_on
+        puts tags[true]
       else
-        fprint "\e[sstarting..." # save cursor
-        
+        fprint "\e[s" + tags[:wait] + " Starting...\e[u" if tty # save/restore cursor
+
         begin
           klass.start
-          
+
           running = klass.running?
           configatron.__send__ "#{entry}=", running
-          puts "\e[u\e[K" + (running ? colored_on : colored_off) # restore & clear to end
+          puts tags[running || :fail] + ' Started    '
         rescue DaemonController::StartTimeout
           configatron.__send__ "#{entry}=", false
-          puts "\e[u\e[K" + (colored_off) + " Failed to start in time"
+          puts tags[:time] + ' Start timed out'
         end
       end
     end
 
     # TODO: Do something about this!
     configatron.ldap.active = false
-    puts "LDAP:               #{configatron.ldap.active ? colored_on : colored_off}"
+    puts "LDAP:               #{tags[configatron.ldap.active || :down]}"
     puts '--------------------------'
   end
 
   def self.stop_all
-    fprint "Shutting down services: \e[s" # save cursor
-    
-    services.each_value do |(name, entry, klass)|
-      fprint "\e[u\e[K#{name}" # restore & clear to end
-      
-      klass.stop if klass.running?
+    if $stdout.tty?
+      fprint "Shutting down services: \e[s" # save cursor
+
+      services.each_value do |(name, entry, klass)|
+        fprint "\e[u\e[K#{name}" # restore & clear to end
+
+        klass.stop if klass.running?
+      end
+
+      fprint "\r\e[2K" # Move to left and clear line
+    else
+      fprint 'Shutting down services... ' rescue nil
+
+      services.each_value do |(name, entry, klass)|
+        klass.stop if klass.running?
+      end
+
+      puts 'done.' rescue nil
     end
-    
-    fprint "\r\e[2K" # Move to left and clear line
   end
 
   def self.fprint *line # print and flush
