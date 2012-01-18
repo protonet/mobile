69772cd5b90f42a533cb9ae2608dd7bcbbd042b4
Ali Jelveh <jelveh@web.de>
2012-01-18 12:10:18 +0100

    * small cleanup in initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,2 @@
 # Be sure to restart your server when you modify this file.
 Dashboard::Application.config.session_store :cookie_store, :key => '_rails_dashboard_session'
-
-# ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, FlashSessionCookieMiddleware, ActionController::Base.session_options[:key])
