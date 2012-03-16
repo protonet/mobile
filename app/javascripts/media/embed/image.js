8c0dfaebc1fdae06043a272b5fed07c92e1d5d85
Christopher Blum <christopher.blum@xing.com>
2012-03-16 12:19:57 +0100

    Make image proxy behave more elegant

--- app/javascripts/media/embed/image.js
+++ app/javascripts/media/embed/image.js
@@ -2,7 +2,8 @@ protonet.media.embed.Image = {
   supportedMimeTypes: ["image/jpeg", "image/png", "image/gif", "image/bmp"],
   
   render: function(src, size) {
-    var $element = $("<img>", { src: protonet.media.Proxy.getImageUrl(src, size) });
+    var options   = $.extent({ extent: false }, size),
+        $element  = $("<img>", { src: protonet.media.Proxy.getImageUrl(src, options) });
     return $element;
   }
 };
