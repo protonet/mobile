eb2710e1845d572deac380a0d813df1d911c20b9
Henning Thies <thies.henning@gmail.com>
2012-03-16 16:15:31 +0100

    fancy invitation improvements

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -7,16 +7,15 @@ class Invitation < ActiveRecord::Base
   validates_format_of :email, :with => Devise.email_regexp
   validates_length_of :channel_ids, :minimum => 1, :message => "must be provided"
   validates_presence_of :token
-  validates_uniqueness_of :email, :on => :create
   
   before_validation :generate_token, :on => :create
   
   scope :unaccepted, :conditions => { :accepted_at => nil }
   
-  validate :check_email
+  validate :check_email, :on => :create
   
   def check_email
-    errors.add :email, "is already taken." if User.find_by_email(email)
+    errors.add :email, "is already taken." if User.find_by_email(email) || Invitation.find_by_email(email)
   end
   
   def send_email
--- lib/protonet_email_service.rb
+++ lib/protonet_email_service.rb
@@ -2,12 +2,12 @@ class ProtonetEmailService
 
   def initialize(settings)
     @settings = settings
-    
   end
 
   def deliver!(mail)
     response = Net::HTTP.post_form(URI.parse("http://email.protonet.info/send"), {
       :license_key => SystemBackend.license_key,
+      :from => mail[:from].formatted,
       :to => mail.to,
       :bcc => mail.bcc,
       :reply_to => mail.reply_to,
