3ef4839ee6d3451556d68fdc80b3291be11f3ee3
Ali Jelveh <jelveh@web.de>
2012-03-27 11:38:08 +0200

    * comments for dnsmasq settings "LET ME GOOGLE THAT FOR YOU"

--- app/models/system_dnsmasq.rb
+++ app/models/system_dnsmasq.rb
@@ -61,12 +61,20 @@ address=/protonet/#{ip}
 address=/#{SystemBackend.hostname}/#{ip}
 address=/#{SystemPreferences.public_host}/#{ip}
 dhcp-range=#{interface},#{ip.network(1)},#{ip.network(32000)},4h
+
+# Gateway
 dhcp-option=3,#{ip}
+
+# DNS
 dhcp-option=6,#{ip}
 
+# IP Forward (no)
 dhcp-option=19,0
+
+# Source Routing
 dhcp-option=20,0
 
+# 44-47 NetBIOS
 dhcp-option=44,0.0.0.0
 dhcp-option=45,0.0.0.0
 dhcp-option=46,8
