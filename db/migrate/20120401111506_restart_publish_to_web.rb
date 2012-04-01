8d9591f53e2a1c8f4b3e1f015d854c1e52fae0a2
Ali Jelveh <jelveh@web.de>
2012-04-01 13:20:49 +0200

    * restart publish to web if it exists

---
+++ db/migrate/20120401111506_restart_publish_to_web.rb
@@ -0,0 +1,16 @@
+class RestartPublishToWeb < ActiveRecord::Migration
+  def self.up
+    if !!SystemPreferences.publish_to_web
+      begin
+        SystemPublishToWeb.unpublish
+        sleep 3
+        SystemPublishToWeb.publish
+      rescue
+        # nothing to do move along...
+      end
+    end
+  end
+
+  def self.down
+  end
+end
