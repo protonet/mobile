9cd62f1a1725356be1eef602b4589a138c6e77f1
prosac <jo@mediaparadise.net>
2012-04-11 17:05:00 +0200

    some docu, added esc key stroke to close console

--- app/javascripts/lib/jquery.quakeStyleConsole/jquery.quakeStyleConsole.js
+++ app/javascripts/lib/jquery.quakeStyleConsole/jquery.quakeStyleConsole.js
@@ -1,9 +1,57 @@
-// quake style console as jQuery-plugin
-// TODO: put in own file. css too (currently in preferences.css)
-// @param function options.onOpen - callback called when opening
-// @param function options.onClose - callback called when pin ponies suck on rainbows
+/*
+quake style console as jQuery-plugin
 
-// based on an alternative approach for writing jquery plugins (http://css-tricks.com/snippets/jquery/jquery-plugin-template/)
+@param int paddingBottom - padding below the content area (optional, defaults to 50px)
+@param int spaceBottom - space below the console in opened state (optional, defaults to 300px)
+@param function options.onopen - callback called when opening
+@param function options.onclose - callback called when pin ponies suck on rainbows
+
+based on an alternative approach for writing jquery plugins (http://css-tricks.com/snippets/jquery/jquery-plugin-template/)
+
+usage:
+... initialize it
+$("#someID").quakeStyleConsole({
+  paddingBottom: 50,
+  spaceBottom: 300
+  onopen: function() {},
+  onclose: function() {}
+});
+... and open it
+$("#someID").quakeStyleConsole.data('quakeStyleConsole').open();
+
+by default click on the html-element and esc key will close the console
+
+the needed html structure
+
+<div id="someID" class="quake-style-console">
+  <!-- the actual content-area -->
+  <div class="quake-style-console-content"></div>
+  <!-- here you can put some other stuff you want to pull down with your console, like buttons or whatever. position it absolute bottom -->
+</div>
+
+the corresponding css styles
+
+.quake-style-console {
+	background-color: rgba(0,0,0,0.8);
+	color: green;
+	font-family: "Lucida Console";
+	left: 0;
+	right: 0;
+	position: fixed;
+	z-index: 1000;
+	top: 0;
+	bottom: 100%;
+}
+.quake-style-console-content {
+	overflow-y: scroll;
+	height: 100%;
+	padding: 0 30px 0 30px;
+}
+.console-box-shadow {
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
+
+*/
 
 (function($) {
   $.quakeStyleConsole = function(el, options) {
@@ -49,6 +97,10 @@
           $("html").bind("click.quakeStyleConsole", function() {
             base.close();
           });
+          $(document).keyup(function(e) {
+            if (e.keyCode == 27) { base.close(); }   // esc
+          });
+          
         }
       );
       base.settings.onopen();
