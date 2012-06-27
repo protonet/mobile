61102a9f9762fb54f9acca4cd3bf6414cd90b4ea
Henning Thies <thies.henning@gmail.com>
2012-06-27 15:33:15 +0200

    fix create_system_channel migration

--- db/migrate/20120620100425_create_system_channel_folders.rb
+++ db/migrate/20120620100425_create_system_channel_folders.rb
@@ -1,7 +1,7 @@
 class CreateSystemChannelFolders < ActiveRecord::Migration
   
   def self.up
-    `mkdir -p #{configatron.files_path}/system_users`
+    `mkdir -m 770 -p #{configatron.files_path}/system_users`
     `chmod g+s #{configatron.files_path}/channels`
     `chmod g+s #{configatron.files_path}/users`
     `chmod g+s #{configatron.files_path}/system_users`
