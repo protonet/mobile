e2894d734c7863e03a32793e0af60aa913dfed14
Benjamin Behr <benny@digitalbehr.de>
2012-06-28 16:44:12 +0200

    app_manager: defaulting instead of raising ;)

--- app/models/app_manager.rb
+++ app/models/app_manager.rb
@@ -13,20 +13,18 @@ module AppManager
 
   class AppProtonetBinding
     class AppKeyNotSpecified < StandardError; end
-    class AppPathNotSpecified < StandardError; end
 
     attr_accessor :app_name, :link_title, :app_path, :app_key, :app_host, :app_port
 
     def initialize(configuration)
       configuration.symbolize_keys!
       raise AppKeyNotSpecified if configuration[:app_key].blank?
-      raise AppPathNotSpecified if configuration[:app_path].blank?
       self.app_name = configuration[:app_name]
       self.app_key = configuration[:app_key]
       self.link_title = configuration[:link_title]
-      self.app_path = configuration[:app_path]
-      self.app_host = configuration[:app_host]
-      self.app_port = configuration[:app_port]
+      self.app_path = configuration[:app_path].blank? ? '/' : configuration[:app_path]
+      self.app_host = configuration[:app_host].blank? ? SystemPreferences.public_host.split(':').first : configuration[:app_host]
+      self.app_port = configuration[:app_port].blank? ? 80 : configuration[:app_port].to_i
     end
 
     def self.new_from_path(binding_file_path)
@@ -39,8 +37,7 @@ module AppManager
     end
 
     def app_url
-      host = local? ? SystemPreferences.public_host.split(':').first : app_host
-      host_with_port = app_port.to_i == 80 ? host : "#{host}:#{app_port}"
+      host_with_port = app_port == 80 ? app_host : "#{app_host}:#{app_port}"
       "http://#{host_with_port}#{app_path}"
     end
   end
