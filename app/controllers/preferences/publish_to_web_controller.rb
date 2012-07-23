f2e435a01e33ececa7cc7fb2ab5bfeebdb998c87
tiff <christopher.blum@spellboy.com>
2012-07-23 15:26:09 +0200

    Start WebPublishing on node setup (untested)

--- app/controllers/preferences/publish_to_web_controller.rb
+++ app/controllers/preferences/publish_to_web_controller.rb
@@ -9,27 +9,21 @@ module Preferences
       end
       
       SystemPreferences.publish_to_web_name = params[:preferences][:publish_to_web_name].downcase
-      SystemPreferences.publish_to_web = setting
       
-      if SystemPreferences.publish_to_web
-        SystemPreferences.public_host = "#{SystemPreferences.publish_to_web_name}.protonet.info"
-        SystemPreferences.public_host_https = true
+      if setting
         turn_off_publishing
         success, error = turn_on_publishing
       else
         turn_off_publishing
       end
+      
       if error
         error_message = JSON.parse(error.message)["errors"].join(", ") rescue error.message
         flash[:error] = "Web Publishing error: " + error_message + "."
       else
-        if SystemPreferences.publish_to_web
-          # to handle local dns resolution for the publish to web domain
-          `/usr/bin/sudo #{configatron.current_file_path}/script/init/published_host update #{SystemPreferences.public_host}`
-          SystemDnsmasq.restart_active
+        if setting
           flash[:notice] = "You successfully published your node to the web"
         else
-          `/usr/bin/sudo #{configatron.current_file_path}/script/init/published_host remove`
           flash[:notice] = "You successfully turned off web publishing"
         end
       end
