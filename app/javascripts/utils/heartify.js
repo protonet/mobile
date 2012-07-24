0e3bbee078db0cf878e14c31392ad89b837237e1
Henning Thies <thies.henning@gmail.com>
2012-07-24 12:13:37 +0200

    smilies are always on!

--- app/javascripts/utils/heartify.js
+++ app/javascripts/utils/heartify.js
@@ -5,7 +5,7 @@ protonet.utils.heartify = (function() {
   var REG_EXP     = /(^|[\s(])&lt;3|\u2665|xxx(?=$|[\s!?.)])/g,
       HTML        = '<span class="heart">&hearts;</span>';
   return function(str, ignoreSetting) {
-    if (!ignoreSetting && !protonet.data.User.getPreference("smilies")) {
+    if (!ignoreSetting) {
       return str;
     }
     return str.replace(REG_EXP, "$1" + HTML);
