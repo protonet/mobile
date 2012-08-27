4428828a187c1395cd88b14ba7d559e2f80eb863
tiff <christopher.blum@spellboy.com>
2012-08-27 14:56:50 +0200

    Forgot to 'git add'

---
+++ test/javascripts/protonet/protonet.js
@@ -0,0 +1,19 @@
+// ---- LIBRARIES
+//= require "../../../app/javascripts/lib/simple-javascript-airbrake-notifier/notifier.js"
+//= require "../../../app/javascripts/lib/jquery-1.7.2.min.js"
+//= require "../../../app/javascripts/lib/jquery-ui-1.8.20.custom.min.js"
+//= require "../../../app/javascripts/lib/swfobject-2.2.js"
+//= require "../../../app/javascripts/lib/jquery-class-create/class.js"
+//= require "../../../app/javascripts/lib/jquery-behaviors/behaviors.js"
+//= require "../../../app/javascripts/lib/jquery.inview/jquery.inview.js"
+//= require "../../../app/javascripts/lib/jquery-patches/src/xhr.js"
+//= require "../../../app/javascripts/lib/jquery-patches/src/event.js"
+
+// ---- TEST LIBRARIES
+
+window.$window   = $(window);
+window.$document = $(document);
+
+// ---- PROTONET
+//= require "../../../app/javascripts/protonet/extensions.js"
+//= require "../../../app/javascripts/protonet/protonet.js"
