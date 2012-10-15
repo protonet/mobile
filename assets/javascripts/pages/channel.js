5f4b8b0698ec84d84db922a930d80d2171674664
Henning Thies <thies.henning@gmail.com>
2012-10-15 17:52:56 +0200

    fix files

--- assets/javascripts/pages/channel.js
+++ assets/javascripts/pages/channel.js
@@ -153,10 +153,10 @@
         .delegate("h1.ui-title", "click", function(event){
           window.scrollTo(0,1);
         })
-        .delegate("a", "click", function(event){
+        .delegate(".ui-header a", "click", function(event){
           event.preventDefault();
         })
-        .delegate("a", "vclick", function(event){
+        .delegate(".ui-header a", "vclick", function(event){
           event.preventDefault();
           event.stopPropagation();
           $.mobile.changePage($('#navigation'),{
